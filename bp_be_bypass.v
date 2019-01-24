c0492872126226bbbe6697c41a67ee0e2ae39504
Dan Petrisko <petrisko@cs.washington.edu>
2019-01-24 13:37:43 -0800

    Update bp_be_bypass.v

--- bp_be_bypass.v
+++ bp_be_bypass.v
@@ -9,7 +9,7 @@
  * Parameters:
  *   fwd_els_p        - Number of potential forwarding candidates. Should be
  *                      equal to # stages in pipeline after dispatch
- *   enable_p         - When disabled, module becomes passthrough. Useful for debugging.
+ *   enable_p         - When disabled, module becomes passthrough. Useful for debugging
  *
  * Inputs:
  *   id_rs1_v_i       - 
@@ -33,33 +33,36 @@
  */
 
 module bp_be_bypass
+ import bp_be_rv64_pkg::*;
  #(parameter fwd_els_p = "inv"
 
-   // Default parameters
+   // Default params
    , parameter enable_p = 1
 
-   // Generated parameters
+   // Generated params
    // # Bypasses == Number of forwarded elements + 1 for the dispatched data
-   , localparam bypass_els_p = fwd_els_p + 1 
+   , localparam bypass_els_lp     = fwd_els_p + 1
+   , localparam reg_addr_width_lp = rv64_reg_addr_width_gp
+   , localparam reg_data_width_lp = rv64_reg_data_width_gp
    )
   (
    // Dispatched instruction operands
-   input logic                                              id_rs1_v_i
-   , input logic[rv64_reg_addr_width_gp-1:0]                id_rs1_addr_i
-   , input logic[rv64_reg_data_width_gp-1:0]                id_rs1_i
+   input logic                                         id_rs1_v_i
+   , input logic[reg_addr_width_lp-1:0]                id_rs1_addr_i
+   , input logic[reg_data_width_lp-1:0]                id_rs1_i
 
-   , input logic                                            id_rs2_v_i
-   , input logic[rv64_reg_addr_width_gp-1:0]                id_rs2_addr_i
-   , input logic[rv64_reg_data_width_gp-1:0]                id_rs2_i
+   , input logic                                       id_rs2_v_i
+   , input logic[reg_addr_width_lp-1:0]                id_rs2_addr_i
+   , input logic[reg_data_width_lp-1:0]                id_rs2_i
 
    // Completed rd writes in the pipeline
-   , input logic[fwd_els_p-1:0]                             fwd_rd_v_i
-   , input logic[fwd_els_p-1:0][rv64_reg_addr_width_gp-1:0] fwd_rd_addr_i
-   , input logic[fwd_els_p-1:0][rv64_reg_data_width_gp-1:0] fwd_rd_i
+   , input logic[fwd_els_p-1:0]                        fwd_rd_v_i
+   , input logic[fwd_els_p-1:0][reg_addr_width_lp-1:0] fwd_rd_addr_i
+   , input logic[fwd_els_p-1:0][reg_data_width_lp-1:0] fwd_rd_i
 
    // The latest valid rs1, rs2 data
-   , output logic[rv64_reg_data_width_gp-1:0]               bypass_rs1_o
-   , output logic[rv64_reg_data_width_gp-1:0]               bypass_rs2_o
+   , output logic[reg_data_width_lp-1:0]               bypass_rs1_o
+   , output logic[reg_data_width_lp-1:0]               bypass_rs2_o
    );
 
 initial begin : parameter_validation
@@ -72,22 +75,21 @@ initial begin : parameter_validation
 end
 
 // Intermediate connections
-logic[bypass_els_p-1:0] rs1_match_vector       , rs2_match_vector;
-logic[bypass_els_p-1:0] rs1_match_vector_onehot, rs2_match_vector_onehot;
-
-logic[bypass_els_p-1:0][rv64_reg_data_width_gp-1:0] rs1_data_vector, rs2_data_vector;
+logic[bypass_els_lp-1:0]                        rs1_match_vector       , rs2_match_vector;
+logic[bypass_els_lp-1:0]                        rs1_match_vector_onehot, rs2_match_vector_onehot;
+logic[bypass_els_lp-1:0][reg_data_width_lp-1:0] rs1_data_vector        , rs2_data_vector;
 
 // Module instantiations
 if(enable_p == 1) begin
   // Find the youngest valid data to forward
-  bsg_priority_encode_one_hot_out #(.width_p(bypass_els_p)
+  bsg_priority_encode_one_hot_out #(.width_p(bypass_els_lp)
                                     ,.lo_to_hi_p(1)
                                     ) match_one_hot_rs1
                                    (.i(rs1_match_vector)
                                     ,.o(rs1_match_vector_onehot)
                                     );
 
-  bsg_priority_encode_one_hot_out #(.width_p(bypass_els_p)
+  bsg_priority_encode_one_hot_out #(.width_p(bypass_els_lp)
                                     ,.lo_to_hi_p(1)
                                     ) match_one_hot_rs2
                                    (.i(rs2_match_vector)
@@ -95,18 +97,18 @@ if(enable_p == 1) begin
                                     );
 
   // Bypass data with a simple crossbar
-  bsg_crossbar_o_by_i #(.i_els_p(bypass_els_p)
+  bsg_crossbar_o_by_i #(.i_els_p(bypass_els_lp)
                         ,.o_els_p(1)
-                        ,.width_p(rv64_reg_data_width_gp)
+                        ,.width_p(reg_data_width_lp)
                         ) rs1_crossbar
                        (.i(rs1_data_vector)
                         ,.sel_oi_one_hot_i(rs1_match_vector_onehot)
                         ,.o(bypass_rs1_o)
                         );
 
-  bsg_crossbar_o_by_i #(.i_els_p(bypass_els_p)
+  bsg_crossbar_o_by_i #(.i_els_p(bypass_els_lp)
                         ,.o_els_p(1)
-                        ,.width_p(rv64_reg_data_width_gp)
+                        ,.width_p(reg_data_width_lp)
                         ) rs2_crossbar
                        (.i(rs2_data_vector)
                         ,.sel_oi_one_hot_i(rs2_match_vector_onehot)
@@ -123,22 +125,22 @@ always_comb begin : vector_generation
   rs1_data_vector = {id_rs1_i, fwd_rd_i};
   rs2_data_vector = {id_rs2_i, fwd_rd_i};
 
-  for(integer i = 0; i < bypass_els_p; i+=1) begin : match_vector
+  for(integer i = 0; i < bypass_els_lp; i+=1) begin : match_vector
     // Dispatched data always matches the dispatched data, otherwise check for:
     // * Register address match 
     // * The completing instruction is writing and the dispatched instruction is reading
-    // * Do not forward X0 data, RISC-V defines this as always 0.
-    rs1_match_vector[i] = ((i == bypass_els_p-1)
+    // * Do not forward x0 data, RISC-V defines this as always 0.
+    rs1_match_vector[i] = ((i == bypass_els_lp-1)
                            || ((id_rs1_addr_i == fwd_rd_addr_i[i])
                                & (id_rs1_v_i & fwd_rd_v_i[i])
-                               & (id_rs1_addr_i != rv64_reg_addr_width_gp'(0))
+                               & (id_rs1_addr_i != reg_addr_width_lp'(0))
                                )
                            );
 
-    rs2_match_vector[i] = ((i == bypass_els_p-1)
+    rs2_match_vector[i] = ((i == bypass_els_lp-1)
                            || ((id_rs2_addr_i == fwd_rd_addr_i[i]) 
                                & (id_rs2_v_i & fwd_rd_v_i[i]) 
-                               & (id_rs2_addr_i != rv64_reg_addr_width_gp'(0))
+                               & (id_rs2_addr_i != reg_addr_width_lp'(0))
                                )
                            );
   end
