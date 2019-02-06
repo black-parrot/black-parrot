6947e66dec46e06e23c0919abcfbc69ee9195e8f
black-parrot <43363264+black-parrot@users.noreply.github.com>
2019-02-06 13:43:33 -0800

    Cleanup be calculator (#2)
    Cleanup calculator for code review

---
+++ bp_be/src/v/bp_be_calculator/bp_be_pipe_int.v
@@ -0,0 +1,110 @@
+/**
+ *
+ * Name:
+ *   bp_be_pipe_int.v
+ * 
+ * Description:
+ *   Pipeline for RISC-V integer instructions. Handles integer computation and mhartid requests.
+ *
+ * Parameters:
+ *
+ * Inputs:
+ *   clk_i            -
+ *   reset_i          -
+ *
+ *   decode_i         - All of the pipeline control information needed for a dispatched instruction
+ *   pc_i             - PC of the dispatched instruction
+ *   rs1_i            - Source register data for the dispatched instruction
+ *   rs2_i            - Source register data for the dispatched instruction
+ *   imm_i            - Immediate data for the dispatched instruction
+ *   exc_i            - Exception information for a dispatched instruction
+ * 
+ *   mhartid_i        - The hartid for this core
+ *
+ * Outputs:
+ *   result_o         - The calculated result of the instruction
+ *   br_tgt_o         - The calculated branch target from branch instructions
+ *   
+ * Keywords:
+ *   calculator, alu, int, integer, rv64i
+ *
+ * Notes:
+ *   
+ */
+module bp_be_pipe_int 
+ import bp_be_rv64_pkg::*;
+ import bp_be_pkg::*;
+ #(// Generated parameters
+   localparam decode_width_lp      = `bp_be_decode_width
+   , localparam exception_width_lp = `bp_be_exception_width
+   , localparam mhartid_width_lp   = `bp_mhartid_width
+   // From RISC-V specifications
+   , localparam reg_data_width_lp = rv64_reg_data_width_gp
+   )
+  (input                            clk_i
+   , input                          reset_i
+
+   // Common pipeline interface
+   , input [decode_width_lp-1:0]    decode_i
+   , input [reg_data_width_lp-1:0]  pc_i
+   , input [reg_data_width_lp-1:0]  rs1_i
+   , input [reg_data_width_lp-1:0]  rs2_i
+   , input [reg_data_width_lp-1:0]  imm_i
+   , input [exception_width_lp-1:0] exc_i
+
+   // For mhartid CSR
+   , input [mhartid_width_lp-1:0]   mhartid_i
+
+   // Pipeline results
+   , output [reg_data_width_lp-1:0] result_o
+   , output [reg_data_width_lp-1:0] br_tgt_o
+   );
+
+// Cast input and output ports 
+bp_be_decode_s     decode;
+bp_be_exception_s  exc;
+
+assign decode = decode_i;
+assign exc    = exc_i;
+
+// Suppress unused signal warnings
+wire unused0 = clk_i;
+wire unused1 = reset_i;
+
+// Submodule connections
+logic [reg_data_width_lp-1:0] src1, src2, baddr, alu_result;
+logic [reg_data_width_lp-1:0] pc_plus4, mhartid;
+
+// Perform the actual ALU computation
+bp_be_int_alu 
+alu
+  (.src1_i(src1)
+   ,.src2_i(src2)
+   ,.op_i(decode.fu_op)
+   ,.opw_v_i(decode.opw_v)
+
+   ,.result_o(alu_result)
+   );
+
+always_comb 
+  begin 
+    src1  = decode.src1_sel  ? pc_i  : rs1_i;
+    src2  = decode.src2_sel  ? imm_i : rs2_i;
+    baddr = decode.baddr_sel ? src1  : pc_i ;
+
+    result_o = decode.mhartid_r_v 
+               ? mhartid 
+               : decode.result_sel
+                 ? pc_plus4
+                 : alu_result;
+  end
+
+always_comb 
+  begin : aux_compute
+    mhartid          = reg_data_width_lp'(mhartid_i);
+    pc_plus4         = pc_i + reg_data_width_lp'(4);
+    br_tgt_o         = baddr + imm_i;
+  end
+
+endmodule : bp_be_pipe_int
+
---
+++ bp_be/src/v/bp_be_calculator/bp_be_pipe_mem.v
@@ -0,0 +1,108 @@
+/**
+ *
+ * Name:
+ *   bp_be_pipe_mem.v
+ * 
+ * Description:
+ *   Pipeline for RISC-V memory instructions. This includes both int + float loads + stores.
+ *
+ * Parameters:
+ *
+ * Inputs:
+ *   clk_i            -
+ *   reset_i          -
+ *
+ *   decode_i         - All of the pipeline control information needed for a dispatched instruction
+ *   pc_i             - PC of the dispatched instruction
+ *   rs1_i            - Source register data for the dispatched instruction
+ *   rs2_i            - Source register data for the dispatched instruction
+ *   imm_i            - Immediate data for the dispatched instruction
+ *   exc_i            - Exception information for a dispatched instruction
+ *
+ *   mmu_resp_i       - Load / store response from the MMU.
+ *   mmu_resp_v_i     - 'ready-then-valid' interface
+ *   mmu_resp_rdy_o   - 
+
+ *
+ * Outputs:
+ *   mmu_cmd_o        -  Load / store command to the MMU
+ *   mmu_cmd_v_o      -  'ready-then-valid' interface
+ *   mmu_cmd_rdy_i    - 
+ * 
+ *   result_o         - The calculated result of a load
+ *   cache_miss_o     - Goes high when the result of the load is a cache miss 
+ *   
+ * Keywords:
+ *   calculator, mem, mmu, load, store, rv64i, rv64f
+ *
+ * Notes:
+ *   
+ */
+
+module bp_be_pipe_mem 
+ import bp_be_rv64_pkg::*;
+ import bp_be_pkg::*;
+ #(// Generated parameters
+   localparam decode_width_lp      = `bp_be_decode_width
+   , localparam exception_width_lp = `bp_be_exception_width
+   , localparam mmu_cmd_width_lp   = `bp_be_mmu_cmd_width
+   , localparam mmu_resp_width_lp  = `bp_be_mmu_resp_width
+
+   // From RISC-V specifications
+   , localparam reg_data_width_lp = rv64_reg_data_width_gp
+   )
+  (input                            clk_i
+   , input                          reset_i
+
+   , input [decode_width_lp-1:0]    decode_i
+   , input [reg_data_width_lp-1:0]  rs1_i
+   , input [reg_data_width_lp-1:0]  rs2_i
+   , input [reg_data_width_lp-1:0]  imm_i
+   , input [exception_width_lp-1:0] exc_i
+
+   , output [mmu_cmd_width_lp-1:0]  mmu_cmd_o
+   , output                         mmu_cmd_v_o
+   , input                          mmu_cmd_rdy_i
+
+   , input [mmu_resp_width_lp-1:0]  mmu_resp_i
+   , input                          mmu_resp_v_i
+   , output                         mmu_resp_rdy_o
+
+   , output [reg_data_width_lp-1:0] result_o
+   , output                         cache_miss_o
+   );
+
+// Cast input and output ports 
+bp_be_decode_s    decode;
+bp_be_exception_s exc;
+bp_be_mmu_cmd_s   mmu_cmd;
+bp_be_mmu_resp_s  mmu_resp;
+
+assign decode    = decode_i;
+assign exc       = exc_i;
+assign mmu_cmd_o = mmu_cmd;
+assign mmu_resp  = mmu_resp_i;
+
+// Suppress unused signal warnings
+wire unused0 = clk_i;
+wire unused1 = reset_i;
+wire unused2 = mmu_cmd_rdy_i;
+wire unused3 = mmu_resp_v_i;
+
+// Module instantiations
+always_comb 
+  begin
+    mmu_cmd.mem_op = decode.fu_op;
+    mmu_cmd.data   = rs2_i;
+    mmu_cmd.addr   = rs1_i + imm_i;
+    mmu_cmd_v_o    = (decode.dcache_r_v | decode.dcache_w_v)
+                     & ~|exc;
+
+    mmu_resp_rdy_o = 1'b1;
+    result_o       = mmu_resp.data;
+
+    cache_miss_o   = mmu_resp.exception.cache_miss_v;
+  end 
+
+endmodule : bp_be_pipe_mem
+
