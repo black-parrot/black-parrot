13918c3d765334e48b106eeb60b8744a6465d637
tommy <dcjung@uw.edu>
2019-02-05 17:14:24 -0800

    whole bunch of struct

--- bp_be/src/v/bp_be_mmu/bp_be_dcache/bp_be_dcache_wbuf.v
+++ bp_be/src/v/bp_be_mmu/bp_be_dcache/bp_be_dcache_wbuf.v
@@ -4,6 +4,10 @@
  *  Data cache write buffer.
  */
 
+// Some notes:
+// - write buffer can store up to two entries.
+
+`include "bp_be_dcache_wbuf_entry.vh"
 
 module bp_be_dcache_wbuf
   #(parameter data_width_p="inv"
@@ -18,23 +22,20 @@ module bp_be_dcache_wbuf
     , localparam block_offset_width_lp=(word_offset_width_lp+byte_offset_width_lp)
     , localparam way_id_width_lp=`BSG_SAFE_CLOG2(ways_p)
     , localparam index_width_lp=`BSG_SAFE_CLOG2(sets_p)
+
+    , localparam wbuf_entry_width_lp=
+      `bp_be_dcache_wbuf_entry_width(paddr_width_p,data_width_p,ways_p)
   )
   (
     input clk_i
     , input reset_i
     
     , input v_i
-    , input [paddr_width_p-1:0] addr_i
-    , input [data_width_p-1:0] data_i
-    , input [data_mask_width_lp-1:0] mask_i
-    , input [way_id_width_lp-1:0] way_i
+    , input [wbuf_entry_width_lp-1:0] wbuf_entry_i
 
-    , output logic v_o
-    , output logic [data_width_p-1:0] data_o
-    , output logic [paddr_width_p-1:0] addr_o
-    , output logic [data_mask_width_lp-1:0] mask_o
-    , output logic [way_id_width_lp-1:0] way_o
     , input yumi_i
+    , output logic v_o
+    , output logic [wbuf_entry_width_lp-1:0] wbuf_entry_o
 
     , output logic empty_o
     
@@ -48,14 +49,13 @@ module bp_be_dcache_wbuf
     , output logic lce_snoop_match_o
   );
 
-  logic [paddr_width_p-1:0] el0_addr;
-  logic [paddr_width_p-1:0] el1_addr;
-  logic [data_width_p-1:0] el0_data;
-  logic [data_width_p-1:0] el1_data;
-  logic [data_mask_width_lp-1:0] el0_mask;
-  logic [data_mask_width_lp-1:0] el1_mask;
-  logic [way_id_width_lp-1:0] el0_way;
-  logic [way_id_width_lp-1:0] el1_way;
+  `declare_bp_be_dcache_wbuf_entry_s(paddr_width_p, data_width_p, ways_p);
+
+  bp_be_dcache_wbuf_entry_s wbuf_entry_in;
+  assign wbuf_entry_in = wbuf_entry_i;
+  
+  bp_be_dcache_wbuf_entry_s wbuf_entry_el0;
+  bp_be_dcache_wbuf_entry_s wbuf_entry_el1;
 
   logic [1:0] num_els_r;
 
@@ -124,53 +124,19 @@ module bp_be_dcache_wbuf
 
   // wbuf queues
   //
-  bp_be_dcache_wbuf_queue #(.width_p(data_mask_width_lp)) wbq_mask (
-    .clk_i(clk_i)
-    ,.data_i(mask_i)
-    ,.el0_en_i(el0_enable)
-    ,.el1_en_i(el1_enable)
-    ,.mux0_sel_i(mux0_sel)
-    ,.mux1_sel_i(mux1_sel)
-    ,.el0_snoop_o(el0_mask)
-    ,.el1_snoop_o(el1_mask)
-    ,.data_o(mask_o)
-  );
-
-  bp_be_dcache_wbuf_queue #(.width_p(data_width_p)) wbq_data (
-    .clk_i(clk_i)
-    ,.data_i(data_i)
-    ,.el0_en_i(el0_enable)
-    ,.el1_en_i(el1_enable)
-    ,.mux0_sel_i(mux0_sel)
-    ,.mux1_sel_i(mux1_sel)
-    ,.el0_snoop_o(el0_data)
-    ,.el1_snoop_o(el1_data)
-    ,.data_o(data_o)
-  );
-
-  bp_be_dcache_wbuf_queue #(.width_p(paddr_width_p)) wbq_addr (
-    .clk_i(clk_i)
-    ,.data_i(addr_i)
-    ,.el0_en_i(el0_enable)
-    ,.el1_en_i(el1_enable)
-    ,.mux0_sel_i(mux0_sel)
-    ,.mux1_sel_i(mux1_sel)
-    ,.el0_snoop_o(el0_addr)
-    ,.el1_snoop_o(el1_addr)
-    ,.data_o(addr_o)
-  );
-
-  bp_be_dcache_wbuf_queue #(.width_p(way_id_width_lp)) wbq_way (
-    .clk_i(clk_i)
-    ,.data_i(way_i)
-    ,.el0_en_i(el0_enable)
-    ,.el1_en_i(el1_enable)
-    ,.mux0_sel_i(mux0_sel)
-    ,.mux1_sel_i(mux1_sel)
-    ,.el0_snoop_o(el0_way)
-    ,.el1_snoop_o(el1_way)
-    ,.data_o(way_o)
-  );
+  bp_be_dcache_wbuf_queue
+    #(.width_p(wbuf_entry_width_lp))
+    wbq
+      (.clk_i(clk_i)
+      ,.data_i(wbuf_entry_in)
+      ,.el0_en_i(el0_enable)
+      ,.el1_en_i(el1_enable)
+      ,.mux0_sel_i(mux0_sel)
+      ,.mux1_sel_i(mux1_sel)
+      ,.el0_snoop_o(wbuf_entry_el0)
+      ,.el1_snoop_o(wbuf_entry_el1)
+      ,.data_o(wbuf_entry_o)
+      );
 
   // bypassing
   //
@@ -180,9 +146,9 @@ module bp_be_dcache_wbuf
   logic [paddr_width_p-byte_offset_width_lp-1:0] bypass_word_addr;
 
   assign bypass_word_addr = bypass_addr_i[paddr_width_p-1:byte_offset_width_lp];
-  assign tag_hit0_n = bypass_word_addr == el0_addr[paddr_width_p-1:byte_offset_width_lp]; 
-  assign tag_hit1_n = bypass_word_addr == el1_addr[paddr_width_p-1:byte_offset_width_lp]; 
-  assign tag_hit2_n = bypass_word_addr == addr_i[paddr_width_p-1:byte_offset_width_lp]; 
+  assign tag_hit0_n = bypass_word_addr == wbuf_entry_el0.paddr[paddr_width_p-1:byte_offset_width_lp]; 
+  assign tag_hit1_n = bypass_word_addr == wbuf_entry_el1.paddr[paddr_width_p-1:byte_offset_width_lp]; 
+  assign tag_hit2_n = bypass_word_addr == wbuf_entry_in.paddr[paddr_width_p-1:byte_offset_width_lp]; 
 
   assign tag_hit0 = tag_hit0_n & el0_valid;
   assign tag_hit1 = tag_hit1_n & el1_valid;
@@ -200,17 +166,17 @@ module bp_be_dcache_wbuf
   logic [data_width_p-1:0] bypass_data_n;
   logic [data_mask_width_lp-1:0] bypass_mask_n;
 
-  assign bypass_mask_n = (tag_hit0x4 & el0_mask)
-    | (tag_hit1x4 & el1_mask)
-    | (tag_hit2x4 & mask_i);
+  assign bypass_mask_n = (tag_hit0x4 & wbuf_entry_el0.mask)
+    | (tag_hit1x4 & wbuf_entry_el1.mask)
+    | (tag_hit2x4 & wbuf_entry_in.mask);
 
   bsg_mux_segmented #(
     .segments_p(data_mask_width_lp)
     ,.segment_width_p(8) 
   ) mux_segmented_merge0 (
-    .data0_i(el1_data)
-    ,.data1_i(el0_data)
-    ,.sel_i(tag_hit0x4 & el0_mask)
+    .data0_i(wbuf_entry_el1.data)
+    ,.data1_i(wbuf_entry_el0.data)
+    ,.sel_i(tag_hit0x4 & wbuf_entry_el0.mask)
     ,.data_o(el0or1_data)
   );
 
@@ -219,8 +185,8 @@ module bp_be_dcache_wbuf
     ,.segment_width_p(8) 
   ) mux_segmented_merge1 (
     .data0_i(el0or1_data)
-    ,.data1_i(data_i)
-    ,.sel_i(tag_hit2x4 & mask_i)
+    ,.data1_i(wbuf_entry_in.data)
+    ,.sel_i(tag_hit2x4 & wbuf_entry_in.mask)
     ,.data_o(bypass_data_n)
   );
 
@@ -243,9 +209,18 @@ module bp_be_dcache_wbuf
   logic lce_snoop_el0_match;
   logic lce_snoop_el1_match;
 
-  assign lce_snoop_el2_match = v_i & (lce_snoop_index_i == addr_i[block_offset_width_lp+:index_width_lp]) & (lce_snoop_way_i == way_i);
-  assign lce_snoop_el0_match = el0_valid & (lce_snoop_index_i == el0_addr[block_offset_width_lp+:index_width_lp]) & (lce_snoop_way_i == el0_way);
-  assign lce_snoop_el1_match = el1_valid & (lce_snoop_index_i == el1_addr[block_offset_width_lp+:index_width_lp]) & (lce_snoop_way_i == el1_way);
+  assign lce_snoop_el2_match = v_i
+    & (lce_snoop_index_i == wbuf_entry_in.paddr[block_offset_width_lp+:index_width_lp])
+    & (lce_snoop_way_i == wbuf_entry_in.way_id);
+
+  assign lce_snoop_el0_match = el0_valid
+    & (lce_snoop_index_i == wbuf_entry_el0.paddr[block_offset_width_lp+:index_width_lp])
+    & (lce_snoop_way_i == wbuf_entry_el0.way_id);
+
+  assign lce_snoop_el1_match = el1_valid
+    & (lce_snoop_index_i == wbuf_entry_el1.paddr[block_offset_width_lp+:index_width_lp])
+    & (lce_snoop_way_i == wbuf_entry_el1.way_id);
+
   assign lce_snoop_match_o = lce_snoop_el2_match | lce_snoop_el0_match | lce_snoop_el1_match;
 
 endmodule
