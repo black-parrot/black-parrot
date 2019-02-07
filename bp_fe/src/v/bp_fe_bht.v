da95e1ea3518ae2d365a9d2a1f136ce5186f7dc3
Zahra Azad <zazad@bu.edu>
2019-02-06 22:33:56 -0500

    initial cleanup pc_gen

--- bp_fe/src/v/bp_fe_bht.v
+++ bp_fe/src/v/bp_fe_bht.v
@@ -8,57 +8,56 @@
  * negative regime, the BHT predicts "not taken". The implementation of BHT is
  * native to this design.
 */
-
-
-`ifndef BSG_DEFINES_V
-`define BSG_DEFINES_V
-`include "bsg_defines.v"
-`endif
-
-module bht 
- #(parameter   saturation_size_lp=2
-   , parameter bht_indx_width_p=5
-   , parameter els_lp=2**bht_indx_width_p
-  )
-  (input logic                          clk_i
-   , input logic                        v_i
-   , input logic                        reset_i
+module bht
+ import bp_fe_pkg::*; 
+ #(localparam saturation_size_lp=2
+   , parameter bht_indx_width_p="inv"
+   , localparam els_lp=2**bht_indx_width_p
+   )
+  (input                          clk_i
+   , input                        en_i
+   , input                        reset_i
     
-   , input logic [bht_indx_width_p-1:0] bht_idx_r_i
-   , input logic [bht_indx_width_p-1:0] bht_idx_w_i
+   , input [bht_indx_width_p-1:0] idx_r_i
+   , input [bht_indx_width_p-1:0] idx_w_i
     
-   , input logic                        bht_r_i
-   , input logic                        bht_w_i
+   , input                        r_v_i
+   , input                        w_v_i
     
-   , input logic                        correct_i
-   , output logic                       predict_o
-  );
+   , input                        correct_i
+   , output                       predict_o
+   );
 
    
 logic [els_lp-1:0][saturation_size_lp-1:0] mem;
 
-assign predict_o = mem[bht_idx_r_i][1];
+assign predict_o = mem[idx_r_i][1];
 
-always_ff @(posedge clk_i) begin
-  if (v_i) begin
-    if (reset_i) begin
-      mem <= '{default:2'b01};
-    end else if (bht_w_i) begin
-    //2-bit saturating counter(high_bit:prediction direction,low_bit:strong/weak prediction)
-      case ({correct_i, mem[bht_idx_w_i][1], mem[bht_idx_w_i][0]})
-        //wrong prediction
-        3'b000: mem[bht_idx_w_i] <= {mem[bht_idx_w_i][1]^mem[bht_idx_w_i][0], 1'b1};//2'b01
-        3'b001: mem[bht_idx_w_i] <= {mem[bht_idx_w_i][1]^mem[bht_idx_w_i][0], 1'b1};//2'b11
-        3'b010: mem[bht_idx_w_i] <= {mem[bht_idx_w_i][1]^mem[bht_idx_w_i][0], 1'b1};//2'b11
-        3'b011: mem[bht_idx_w_i] <= {mem[bht_idx_w_i][1]^mem[bht_idx_w_i][0], 1'b1};//2'b01
-        //correct prediction
-        3'b100: mem[bht_idx_w_i] <= mem[bht_idx_w_i];//2'b00
-        3'b101: mem[bht_idx_w_i] <= {mem[bht_idx_w_i][1], ~mem[bht_idx_w_i][0]};//2'b00
-        3'b110: mem[bht_idx_w_i] <= mem[bht_idx_w_i];//2'b10
-        3'b111: mem[bht_idx_w_i] <= {mem[bht_idx_w_i][1], ~mem[bht_idx_w_i][0]};//2'b10
-      endcase
-    end
+always_ff @(posedge clk_i) 
+  begin
+    if (en_i) 
+      begin
+        if (reset_i) 
+          begin
+            mem <= '{default:2'b01};
+          end 
+        else if (w_v_i) 
+          begin
+            //2-bit saturating counter(high_bit:prediction direction,low_bit:strong/weak prediction)
+            case ({correct_i, mem[idx_w_i][1], mem[idx_w_i][0]})
+              //wrong prediction
+              3'b000: mem[idx_w_i] <= {mem[idx_w_i][1]^mem[idx_w_i][0], 1'b1};//2'b01
+              3'b001: mem[idx_w_i] <= {mem[idx_w_i][1]^mem[idx_w_i][0], 1'b1};//2'b11
+              3'b010: mem[idx_w_i] <= {mem[idx_w_i][1]^mem[idx_w_i][0], 1'b1};//2'b11
+              3'b011: mem[idx_w_i] <= {mem[idx_w_i][1]^mem[idx_w_i][0], 1'b1};//2'b01
+              //correct prediction
+              3'b100: mem[idx_w_i] <= mem[idx_w_i];//2'b00
+              3'b101: mem[idx_w_i] <= {mem[idx_w_i][1], ~mem[idx_w_i][0]};//2'b00
+              3'b110: mem[idx_w_i] <= mem[idx_w_i];//2'b10
+              3'b111: mem[idx_w_i] <= {mem[idx_w_i][1], ~mem[idx_w_i][0]};//2'b10
+            endcase
+          end
+      end
   end
-end
 
 endmodule
