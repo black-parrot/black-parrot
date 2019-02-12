f8b73e267dc02460149465f0b577b02496906d4c
Mark Wyse <muwyse@gmail.com>
2019-02-11 22:36:56 -0800

    use ME ROM instead of BE ROM for mem
    Former-commit-id: b167cf068085a4f195b218fa1bfa78b4855519f1

--- bp_me/src/v/roms/bp_me_mem_rom_1.v
+++ bp_me/src/v/roms/bp_me_mem_rom_1.v
@@ -1,5 +1,5 @@
 // This ROM is empty and should be replaced by an actual test ROM via Makefile
-module bp_be_boot_rom #(parameter width_p=-1, addr_width_p=-1)
+module bp_me_boot_rom #(parameter width_p=-1, addr_width_p=-1)
 (input [addr_width_p-1:0] addr_i
  ,output logic [width_p-1:0] data_o
  );
