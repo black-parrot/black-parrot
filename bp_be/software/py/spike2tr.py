b396fd1dbfdfee8466d0ca2f9ea4da969039b86c
Dan Petrisko <petrisko@cs.washington.edu>
2019-02-19 20:40:18 -0800

    Unifying makefiles for be and top
    Former-commit-id: f657f5562fc1333c7d084b03f867db0d322a8f05

--- bp_be/software/py/spike2tr.py
+++ bp_be/software/py/spike2tr.py
@@ -11,7 +11,10 @@ def hex2bin(hex, width):
 
 def sendBinary(send):
 	binary = '# ' + str(send[2]) + '\n'
-	binary += "0001_" + hex2bin(send[0], 16*4) + '_' + hex2bin(send[1], 8*4)
+	binary += "0001_" 
+	for i in xrange(33):
+		binary += '0'
+	binary += '_' + hex2bin(send[0], 16*4) + '_' + hex2bin(send[1], 8*4)
 	return binary
 
 def recvBinary(recv):
@@ -21,11 +24,25 @@ def recvBinary(recv):
 		binary += "0010_1_" + hex2bin(recv[1], 16*4) + '_' + hex2bin(recv[2], 16*4)
 	else:
 		binary = '# check r' + str(recv[1]) + ' == ' + recv[2] + '\n'
-		binary += "0010_0_" + int2bin(recv[1], 5) + '_' + hex2bin(recv[2], 16*4)
+		binary += "0010_0_" 
+		for i in xrange(64-5):
+			binary += '0'
+		binary += '_' + int2bin(recv[1], 5) + '_' + hex2bin(recv[2], 16*4)
 	return binary
 
+def tr_finish():
+  binary  = '# Finish' + '\n'
+  binary += '0100_0_' + hex2bin(str(0), 64) + '_' + hex2bin(str(0), 64)
+  return binary
+
+def tr_done():
+  binary  = '# Done' + '\n'
+  binary += '0011_0_' + hex2bin(str(0), 64) + '_' + hex2bin(str(0), 64)
+  return binary
+
+
 name = str(sys.argv[1])
-recv_els = int(sys.argv[2])
+#recv_els = int(sys.argv[2])
 infile = open(name + ".spike", "r")
 outfile = open(name + ".tr", "w")
 
@@ -37,18 +54,27 @@ outfile.write("# Memory write:		0010_1_addr(64 bit)_data(64 bit) \n")
 send = []
 recv = []
 
+start = 0
 lines = infile.readlines()
 
 for i in xrange(len(lines)):
 	line = lines[i].rstrip("\n\r").split()
 	
+	if("ecall" in line):
+		break
 	if(line[0] == "core" and line[2][:2] == "0x"):
+		skip = 0
 		pc = line[2]
 		instr_hex = line[3][1:-1]
 		instr_str = ''
 		for k in xrange(len(line[4:])):
 			instr_str = instr_str + str(line[4+k]) + ' '
-		send.append([pc, instr_hex, instr_str])
+		if(pc == "0x0000000080000124"):
+			start = 1
+		if(instr_str[:3] == "csr"):
+			skip = 1;
+		if(start and (not skip)):
+			send.append([pc, instr_hex, instr_str])
 	
 	if(line[0] == "rd"):
 		if(len(line[2]) == 1):
@@ -57,10 +83,12 @@ for i in xrange(len(lines)):
 		else:
 			regNum = int(line[2][1:])
 			wrData = line[3]
-		recv.append(['0', regNum, wrData])
+		if(start and (not skip)):
+			recv.append(['0', regNum, wrData])
 		
 	if(line[0] == "mem" and line[2] == 's'):
-		recv.append(['1', line[1], line[3]])
+		if(start and (not skip)):
+			recv.append(['1', line[1], line[3]])
 		
 #	if(len(recv) == recv_els or i == (len(lines) - 1)):
 #		for i in xrange(len(send)):
@@ -71,12 +99,8 @@ for i in xrange(len(lines)):
 #		recv = []
 
 
-for i in xrange(len(send)):
-	outfile.write(sendBinary(send[i]) + '\n')
+for i in xrange(len(recv)):
+	outfile.write(recvBinary(recv[i]) + '\n')
 
+outfile.write(tr_done() + '\n')
 outfile.close()
-		
-	
-	
-	
-	
