1690b599338e050122f2044898c6eb35b97eaf9f
tommie <46542701+tommydcjung@users.noreply.github.com>
2019-03-18 16:43:20 -0700

    synth fixes (#111)
    * synth fixes
    
    * bug fix in FSM

--- bp_be/test/common/bp_be_nonsynth_tracer.v
+++ bp_be/test/common/bp_be_nonsynth_tracer.v
@@ -8,6 +8,7 @@
 module bp_be_nonsynth_tracer
  /* TODO: Get rid of this */
  import bp_be_pkg::*;
+ import bp_common_pkg::*;
  import bp_be_rv64_pkg::*;
  #(parameter vaddr_width_p="inv"
    , parameter paddr_width_p="inv"
