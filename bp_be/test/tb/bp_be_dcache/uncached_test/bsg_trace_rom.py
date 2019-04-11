a09ecbfd81dc948f0459f59e79cf4266f6f8c982
Tommy Jung <dcjung@uw.edu>
2019-04-11 09:13:25 -0700

    sending loopback

--- bp_be/test/tb/bp_be_dcache/uncached_test/bsg_trace_rom.py
+++ bp_be/test/tb/bp_be_dcache/uncached_test/bsg_trace_rom.py
@@ -1,13 +1,18 @@
 import sys
 sys.path.append("../py")
 from trace_gen import TraceGen
+from npa_addr_gen import NPAAddrGen
+
+
 
 tg = TraceGen(addr_width_p=39, data_width_p=64)
+npa = NPAAddrGen(y_cord_width_p=1, x_cord_width_p=2, epa_addr_width_p=12)
 
 tg.print_header()
 
-tg.send_store(size=8, addr=(0 | (1 << 38)), data=1234567890)
-tg.send_load(size=8, addr=(0 | (1 << 38)), signed=0)
+npa_addr = npa.get_npa_addr(y=1,x=0,epa_addr=0)
+tg.send_store(size=8, addr=(npa_addr | (1 << 38)), data=1234567890)
+tg.send_load(size=8, addr=(npa_addr | (1 << 38)), signed=0)
 tg.recv_data(data=0)
 tg.recv_data(data=1234567890)
 
