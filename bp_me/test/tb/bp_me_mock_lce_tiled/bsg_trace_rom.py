69c7d8ec3b7ea63ee66ad1b6649198002c305676
Mark Wyse <muwyse@gmail.com>
2019-05-14 18:57:18 -0700

    narrow testing for bug hunting

--- bp_me/test/tb/bp_me_mock_lce_tiled/bsg_trace_rom.py
+++ bp_me/test/tb/bp_me_mock_lce_tiled/bsg_trace_rom.py
@@ -25,8 +25,12 @@ store_val = num_lce_p if id_p == 0 else id_p
 
 for i in range(num_instr):
   load_not_store = random.randint(0,1)
-  tag = random.randint(0,255) << 6
+  #tag = random.randint(0,255) << 12
+  #tag = random.randint(0,7) << 12
+  tag = random.randint(0,1) << 12
+  #block = random.randint(0,63) << 6
   block_offset = random.randint(0,7) << 3
+  #addr = tag + block + block_offset
   addr = tag + block_offset
   if (load_not_store):
     tg.send_load(size=8, addr=addr, signed=0)
