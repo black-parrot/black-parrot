371e6aadbcfe6e2919defcc850b54930ea645e54
Mark Wyse <muwyse@gmail.com>
2019-06-07 16:06:44 -0700

    update bp_top for CCE concurrency

--- bp_top/test/tb/bp_top_trace_demo/testbench.v
+++ bp_top/test/tb/bp_top_trace_demo/testbench.v
@@ -12,7 +12,8 @@ module testbench
  import bp_cce_pkg::*;
  #(parameter bp_cfg_e cfg_p = BP_CFG_FLOWVAR // Replaced by the flow with a specific bp_cfg
    `declare_bp_proc_params(cfg_p)
-   `declare_bp_me_if_widths(paddr_width_p, cce_block_width_p, num_lce_p, lce_assoc_p)
+   , localparam cce_mshr_width_lp = `bp_cce_mshr_width(num_lce_p, lce_assoc_p, paddr_width_p)
+   `declare_bp_me_if_widths(paddr_width_p, cce_block_width_p, num_lce_p, lce_assoc_p, cce_mshr_width_lp)
 
    // Number of elements in the fake BlackParrot memory
    , parameter clock_period_in_ps_p = 1000
