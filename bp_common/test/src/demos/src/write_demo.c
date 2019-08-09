78443024fda47859e0f1a70860d3c2bd1ac44379
Dan Petrisko <petrisko@cs.washington.edu>
2019-08-09 12:05:51 -0700

    ../../bp_common/test/src/demos/src/write_demo.c

--- bp_common/test/src/demos/src/write_demo.c
+++ bp_common/test/src/demos/src/write_demo.c
@@ -22,6 +22,7 @@ uint64_t main(uint64_t argc, char * argv[]) {
       sum += MATRIX[i*NUM_CORES + core_id];
     }
 
-    bp_barrier_end(&end_barrier_mem, NUM_CORES);
+    bp_finish(0);
+
     return 0;
 }
