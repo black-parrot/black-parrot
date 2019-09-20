5716f2297d7f1e0c64dd0a70f6a5060f7b857477
Dan Petrisko <petrisko@cs.washington.edu>
2019-09-20 15:53:52 -0700

    Cleaning up ci script

---
+++ ci/regress.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CI_CORES=${CI_CORES:-1}
+
+echo "Updating hardware libraries"
+make update_libs
+
+echo "Updating CCE ucode"
+make ucode
+
+echo "Executing regression $1 on end $2"
+make -C $2/syn $1 -j $CI_CORES
+
+grep -cr "FAILED" $2/syn/reports/ && exit 1
+exit 0
+
