9124ec87bee5bb360be86b4ab3c58962bb1f5dd7
Farzam Gilani <gilanifarzam@gmail.com>
2019-11-13 18:34:49 -0800

    Minor fixes to last commit

--- bp_be/src/v/bp_be_checker/bp_be_scheduler.v
+++ bp_be/src/v/bp_be_checker/bp_be_scheduler.v
@@ -218,7 +218,7 @@ bp_be_dispatch_pkt_s dispatch_pkt;
 always_comb
   begin
     // Calculator status ISD stage
-    isd_status.isd_v        = issue_pkt_v_r & ~poison_iss_r & ~poison_iss_i;
+    isd_status.isd_v        = issue_pkt_v_r & ~poison_iss_r & ~poison_iss_i & ~accept_irq_i;
     isd_status.isd_pc       = issue_pkt_r.pc;
     isd_status.isd_branch_metadata_fwd = issue_pkt_r.branch_metadata_fwd;
     isd_status.isd_irq_v    = accept_irq_i;
@@ -232,8 +232,8 @@ always_comb
     isd_status.isd_rs2_addr = issue_pkt_r.instr.fields.rtype.rs2_addr;
 
     // Form dispatch packet
-    dispatch_pkt.v      = issue_pkt_v_r & dispatch_v_i;
-    dispatch_pkt.poison = poison_iss_r | npc_mismatch;
+    dispatch_pkt.v      = (issue_pkt_v_r | accept_irq_i) & dispatch_v_i;
+    dispatch_pkt.poison = ~accept_irq_i & (poison_iss_r | npc_mismatch);
     dispatch_pkt.pc     = expected_npc_i;
     dispatch_pkt.instr  = issue_pkt_r.instr;
     dispatch_pkt.rs1    = irf_rs1; // TODO: Add float forwarding
