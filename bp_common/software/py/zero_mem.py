2de4413230caac6c1af9eda8e92a28a88beedcce
Dan Petrisko <petrisko@cs.washington.edu>
2019-11-23 06:22:05 -0800

    Bumping riscv-tests

--- bp_common/software/py/zero_mem.py
+++ bp_common/software/py/zero_mem.py
@@ -2,11 +2,11 @@ from __future__ import print_function
 
 N = 1024*1024
 cols = 16
-rows = N/cols
+rows = N//cols
 
 print('@0')
-for i in xrange(rows):
-  for j in xrange(cols-1):
+for i in range(rows):
+  for j in range(cols-1):
     print('00 ', end='')
   print('00')
 
