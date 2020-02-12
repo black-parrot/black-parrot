ce2de00c82fea77cf56bae967994eca275d681d5
Paul Gao <gaozihou1@gmail.com>
2020-02-12 15:48:36 -0800

    add comments

--- bp_top/test/tb/bp_top_fpga/modify_tcl.sh
+++ bp_top/test/tb/bp_top_fpga/modify_tcl.sh
@@ -1,7 +1,9 @@
-
+# Ignore .prj files, later added automatically during DB generation
+# (Resolving bug in Vivado tcl generation)
 sed -i '/set file .*\/.*\.prj/,+2d' ./bp_fpga.tcl
 sed -i '/\/.*\.prj/d' ./bp_fpga.tcl
 
+# Add files to project instead of importing (copying) to project directory
 sed -i 's/imported_files/added_files/g' ./bp_fpga.tcl
 sed -i 's/file_imported/file_added/g' ./bp_fpga.tcl
 sed -i 's/import_files/add_files/g' ./bp_fpga.tcl
