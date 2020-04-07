8f181b691323847a0fb05cf701a5a67b297c229c
Farzam Gilani <gilanifarzam@gmail.com>
2020-04-07 11:40:36 -0700

    Fixed sign extension in mul pipe

--- bp_be/src/v/bp_be_calculator/bp_be_pipe_mul.v
+++ bp_be/src/v/bp_be_calculator/bp_be_pipe_mul.v
@@ -40,15 +40,15 @@ wire opw_li = decode.opw_v;
 
 // TODO: Gate FU with pipe_mul_v
 
-wire [reg_data_width_lp-1:0] src1_w_sgn = $signed(rs1_i[0+:word_width_lp]);
-wire [reg_data_width_lp-1:0] src2_w_sgn = $signed(rs2_i[0+:word_width_lp]);
+wire [reg_data_width_lp-1:0] src1_w_sgn = reg_data_width_lp'($signed(rs1_i[0+:word_width_lp]));
+wire [reg_data_width_lp-1:0] src2_w_sgn = reg_data_width_lp'($signed(rs2_i[0+:word_width_lp]));
 
 wire [reg_data_width_lp-1:0] op_a = opw_li ? src1_w_sgn : rs1_i;
 wire [reg_data_width_lp-1:0] op_b = opw_li ? src2_w_sgn : rs2_i;
 
 wire [reg_data_width_lp-1:0] full_result = op_a * op_b;
 
-wire [reg_data_width_lp-1:0] mul_lo = opw_li ? $signed(full_result[0+:word_width_lp]) : full_result;
+wire [reg_data_width_lp-1:0] mul_lo = opw_li ? reg_data_width_lp'($signed(full_result[0+:word_width_lp])) : full_result;
 
 bsg_dff_chain
  #(.width_p(reg_data_width_lp)
