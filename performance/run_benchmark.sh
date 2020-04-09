3507f457d44cf98e0a0c6ffd084fc85d0cce9bc2
Dan Petrisko <petrisko@cs.washington.edu>
2020-04-09 08:10:28 -0400

    Update run_benchmark.sh

--- performance/run_benchmark.sh
+++ performance/run_benchmark.sh
@@ -23,11 +23,13 @@ rm -f reports/${BENCH}.*
 rm -f traces/${BENCH}.*
 rm -f graphs/${BENCH}.*
 
+rm -f $TOP/bp_common/test/$BENCH.dromajo.*
+
 echo "Creating checkpoints of size: $CPSZ for benchmark: $BENCH"  | tee -a $BENCH.log
-#make -C $TOP/bp_common/test $BENCH.dromajo DUMP_PERIOD=$CPSZ > logs/$BENCH.checkpoint.log 2>&1
+make -C $TOP/bp_common/test $BENCH.dromajo DUMP_PERIOD=$CPSZ > logs/$BENCH.checkpoint.log 2>&1
 
 echo "Building the cosimulation model with warmup: $WMSZ and execution: $RNSZ" | tee -a $BENCH.log
-#make -C $TOP/bp_top/syn build.v COSIM_P=1 LOAD_NBF_P=1 WARMUP_INSTR_P=$WMSZ COSIM_INSTR_P=$RNSZ > logs/$BENCH.build.log 2>&1
+make -C $TOP/bp_top/syn build.v COSIM_P=1 LOAD_NBF_P=1 WARMUP_INSTR_P=$WMSZ COSIM_INSTR_P=$RNSZ > logs/$BENCH.build.log 2>&1
 
 echo "Generating checkpoint run commands" | tee -a $BENCH.log
 checkpoints=()
@@ -36,16 +38,9 @@ for f in $(ls $TOP/bp_common/test/mem/$BENCH.dromajo.*.mem); do
 done
 
 # Cap threads at max num checkpoints
-N=$(($N<${#checkpoints[@]} ? $N : ${#checkpoints[@]}))
 echo "Executing commands on $N threads" | tee -a $BENCH.log
 cmd_base="make -C $TOP/bp_top/syn sim.v "
-for c in ${checkpoints[@]}; do
-    # Execute up to N at a time
-    ((i=i%N)); ((i++==0)) && wait
-    cmd="$cmd_base PROG=$c"
-    echo "Executing $cmd" | tee -a $BENCH.log
-    #eval $cmd > logs/$c.log 2>&1 &
-done
+parallel --jobs $N --results logs --progress "$cmd_base PROG={}" ::: "${checkpoints[@]}"
 
 echo "Extracting results" | tee -a $BENCH.log
 for c in ${checkpoints[@]}; do
