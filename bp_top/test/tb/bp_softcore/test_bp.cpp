e8b0731753802cffe79d0fe5abecf0f257871bbd
Dan Petrisko <petrisko@cs.washington.edu>
2020-04-27 15:57:18 -0700

    Revamping test flow

--- bp_top/test/tb/bp_softcore/test_bp.cpp
+++ bp_top/test/tb/bp_softcore/test_bp.cpp
@@ -28,6 +28,7 @@ int sc_main(int argc, char **argv)
   tb->reset_i(reset);
 
 #if VM_TRACE
+  std::cout << "Opening dump file" << std::endl;
   VerilatedVcdSc* wf = new VerilatedVcdSc;
   tb->trace(wf, 10);
   wf->open("dump.vcd");
@@ -35,9 +36,11 @@ int sc_main(int argc, char **argv)
 
   reset = 1;
 
+  std::cout << "Raising reset" << std::endl;
   for (int i = 0; i < 20; i++) {
     sc_start(BP_SIM_CLK_PERIOD, SC_NS);
   }
+  std::cout << "Lowering reset" << std::endl;
 
   reset = 0;
   Verilated::assertOn(true);
@@ -45,10 +48,13 @@ int sc_main(int argc, char **argv)
   while (!Verilated::gotFinish()) {
     sc_start(BP_SIM_CLK_PERIOD, SC_NS);
   }
+  std::cout << "Finishing test" << std::endl;
 
 #if VM_COVERAGE
-  VerilatedCov::write(argv[1]);
+  std::cout << "Writing coverage" << std::endl;
+  VerilatedCov::write("coverage.dat");
 #endif
 
+  std::cout << "Exiting" << std::endl;
   exit(EXIT_SUCCESS);
 }
