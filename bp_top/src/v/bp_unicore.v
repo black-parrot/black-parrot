f68b8fcb47d60f7da2f1bc793e24b5bb58c2fbfd
Shashank-Vijay <shashankvijayaranga@gmail.com>
2020-06-06 18:09:00 -0700

    Adding domain enabling support to mmio_cfg_loader, some interface signal additions

--- bp_top/src/v/bp_unicore.v
+++ bp_top/src/v/bp_unicore.v
@@ -123,6 +123,7 @@ module bp_unicore
   logic [vaddr_width_p-1:0] cfg_npc_data_li;
   logic [dword_width_p-1:0] cfg_csr_data_li;
   logic [1:0]               cfg_priv_data_li;
+  logic [7:0]               cfg_domain_data_li;
   logic [cce_instr_width_p-1:0] cfg_cce_ucode_data_li;
 
   bp_core_minimal
@@ -136,6 +137,7 @@ module bp_unicore
      ,.cfg_irf_data_o(cfg_irf_data_li)
      ,.cfg_csr_data_o(cfg_csr_data_li)
      ,.cfg_priv_data_o(cfg_priv_data_li)
+     ,.cfg_domain_data_o(cfg_domain_data_li)
 
      ,.dcache_req_o(dcache_req_lo)
      ,.dcache_req_v_o(dcache_req_v_lo)
@@ -330,6 +332,7 @@ module bp_unicore
      ,.npc_data_i(cfg_npc_data_li)
      ,.csr_data_i(cfg_csr_data_li)
      ,.priv_data_i(cfg_priv_data_li)
+     ,.domain_data_i(cfg_domain_data_li)
      ,.cce_ucode_data_i('0)
      );
 
