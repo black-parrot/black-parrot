9ed2b4956519c4b2e0da232629a7b2e776e4c813
sripathi-muralitharan <sripathimuralitharan@gmail.com>
2020-06-10 20:03:46 -0700

    Fixing review comments

--- bp_common/src/v/bp_pma.v
+++ bp_common/src/v/bp_pma.v
@@ -5,21 +5,25 @@ module bp_pma
  import bp_common_cfg_link_pkg::*;
  #(parameter bp_params_e bp_params_p = e_bp_inv_cfg
    `declare_bp_proc_params(bp_params_p)
+
+   , localparam cfg_bus_width_lp = `bp_cfg_bus_width(vaddr_width_p, core_id_width_p, cce_id_width_p, lce_id_width_p, cce_pc_width_p, cce_instr_width_p)
    )
-  (input                      clk_i
-   , input                    reset_i
+  (input                          clk_i
+   , input                        reset_i
+   , input [cfg_bus_width_lp-1:0] cfg_bus_i
+  
+   , input                        ptag_v_i
+   , input [ptag_width_p-1:0]     ptag_i
 
-   , input                    ptag_v_i
-   , input [ptag_width_p-1:0] ptag_i
-   , input [7:0]              domain_i
-   , input                    domain_w_v_i
-   , input                    sac_i
-   , input                    sac_w_v_i
-
-   , output                   uncached_o
-   , output [7:0]             domain_data_o
-   , output                   sac_data_o
+   , output                       uncached_o
+   , output [7:0]                 domain_data_o
+   , output                       sac_data_o
    );
+
+  `declare_bp_cfg_bus_s(vaddr_width_p, core_id_width_p, cce_id_width_p, lce_id_width_p, cce_instr_width_p);
+  
+  bp_cfg_bus_s cfg_bus_cast_i;
+  assign cfg_bus_cast_i = cfg_bus_i;
    
   wire is_local_addr = (ptag_i < (dram_base_addr_gp >> page_offset_width_p));
   wire is_io_addr    = (ptag_i[ptag_width_p-1-:io_noc_did_width_p] != '0);
@@ -37,9 +41,8 @@ module bp_pma
      domain_reg
      (.clk_i(clk_i)
      ,.reset_i(reset_i)
-     ,.en_i(domain_w_v_i)
-     // We want the 0th domain to be enabled always
-     ,.data_i((domain_i | 8'h1))
+     ,.en_i(cfg_bus_cast_i.domain_w_v)
+     ,.data_i(cfg_bus_cast_i.domain)
      ,.data_o(domain_data_r)
      );
 
@@ -51,12 +54,13 @@ module bp_pma
      sac_reg
      (.clk_i(clk_i)
      ,.reset_i(reset_i)
-     ,.en_i(sac_w_v_i)
-     ,.data_i(sac_i)
+     ,.en_i(cfg_bus_cast_i.sac_w_v)
+     ,.data_i(cfg_bus_cast_i.sac)
      ,.data_o(sac_data_r)
      );
 
-  assign domain_data_o = domain_data_r;
+  // We want the 0th domain to be enabled always
+  assign domain_data_o = domain_data_r | 8'h1;
   assign sac_data_o = sac_data_r;
 
   assign uncached_o = ptag_v_i & (is_local_addr | is_io_addr);
