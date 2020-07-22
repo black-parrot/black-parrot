31de69ece686827665b79f50515a10d24666ace6
Farzam Gilani <gilanifarzam@gmail.com>
2020-07-22 14:52:37 -0700

    Adapter refactor and adding DRAM clock domain

--- bp_me/test/common/bp_mem.v
+++ bp_me/test/common/bp_mem.v
@@ -45,34 +45,13 @@ module bp_mem
    , input                               mem_resp_yumi_i
    );
 
-`declare_bsg_cache_dma_pkt_s(paddr_width_p);
-
-logic                     dma_pkt_v_lo, dma_data_v_lo, dma_data_v_li;
-logic                     dma_pkt_yumi_li, dma_data_yumi_li, dma_data_ready_lo;
-bsg_cache_dma_pkt_s       dma_pkt_lo;
-logic [`dram_pkg::data_width_p-1:0] dma_data_lo, dma_data_li;
-logic [(`dram_pkg::data_width_p >> 3)-1:0] dma_mask_lo;
-logic [paddr_width_p-1:0] dma_addr_li;
+logic dram_clk, dram_reset;
 
 logic [`dram_pkg::num_channels_p-1:0] dram_v_li, dram_w_li, dram_data_v_li, dram_data_v_lo;
 logic [`dram_pkg::num_channels_p-1:0] dram_yumi_lo, dram_data_yumi_lo;
 logic [`dram_pkg::num_channels_p-1:0][`dram_pkg::channel_addr_width_p] dram_ch_addr_li, dram_read_done_ch_addr_lo;
 logic [`dram_pkg::num_channels_p-1:0][`dram_pkg::data_width_p-1:0] dram_data_li, dram_data_lo;
 logic [`dram_pkg::num_channels_p-1:0][(`dram_pkg::data_width_p >> 3)-1:0] dram_mask_li;
-
-assign dram_v_li[0] = dma_pkt_v_lo;
-assign dram_w_li[0] = dma_pkt_lo.write_not_read;
-assign dram_data_v_li[0] = dma_data_v_lo;
-assign dma_data_v_li = dram_data_v_lo[0];
-
-assign dma_pkt_yumi_li = dram_yumi_lo[0];
-assign dma_data_yumi_li = dram_data_yumi_lo[0];
-
-assign dram_ch_addr_li[0] = dma_pkt_lo.addr;
-assign dram_data_li[0] = dma_data_lo;
-assign dram_mask_li[0] = dma_mask_lo;
-assign dma_data_li = dram_data_lo[0];
-assign dma_addr_li = paddr_width_p'(dram_read_done_ch_addr_lo[0]);
 
 genvar i;
 for (i=1; i<`dram_pkg::num_channels_p; i=i+1) begin
@@ -83,27 +62,16 @@ end
 
 bp_mem_to_dram
   #(.bp_params_p(bp_params_p)
+    ,.channel_addr_width_p(`dram_pkg::channel_addr_width_p)
     ,.data_width_p(`dram_pkg::data_width_p)
     ,.dram_base_p(mem_offset_p)
     ,.fifo_els_p(16)
    )
-  cce2dma
+  mem2dram
    (.clk_i(clk_i)
-    ,.reset_i(reset_i)
-
-    ,.dma_pkt_o(dma_pkt_lo)
-    ,.dma_pkt_v_o(dma_pkt_v_lo)
-    ,.dma_pkt_yumi_i(dma_pkt_yumi_li)
-
-    ,.dma_data_o(dma_data_lo)
-    ,.dma_mask_o(dma_mask_lo)
-    ,.dma_data_v_o(dma_data_v_lo)
-    ,.dma_data_yumi_i(dma_data_yumi_li)
-
-    ,.dma_data_i(dma_data_li)
-    ,.dma_addr_i(dma_addr_li)
-    ,.dma_data_v_i(dma_data_v_li)
-    ,.dma_data_ready_o(dma_data_ready_lo)
+    // We have to make sure reset is 1
+    // on the first posedge of both clocks
+    ,.reset_i(reset_i | dram_reset)
 
     ,.mem_cmd_i(mem_cmd_i)
     ,.mem_cmd_v_i(mem_cmd_v_i)
@@ -112,7 +80,25 @@ bp_mem_to_dram
     ,.mem_resp_o(mem_resp_o)
     ,.mem_resp_v_o(mem_resp_v_o)
     ,.mem_resp_yumi_i(mem_resp_yumi_i)
-    );
+
+    ,.dram_clk_i(dram_clk)
+    ,.dram_reset_i(dram_reset)
+
+    ,.dram_ch_addr_o(dram_ch_addr_li[0])
+    ,.dram_write_not_read_o(dram_w_li[0])
+    ,.dram_v_o(dram_v_li[0])
+    ,.dram_yumi_i(dram_yumi_lo[0])
+
+    ,.dram_data_o(dram_data_li[0])
+    ,.dram_mask_o(dram_mask_li[0])
+    ,.dram_data_v_o(dram_data_v_li[0])
+    ,.dram_data_yumi_i(dram_data_yumi_lo[0])
+
+    ,.dram_data_i(dram_data_lo[0])
+    ,.dram_ch_addr_i(dram_read_done_ch_addr_lo[0])
+    ,.dram_data_v_i(dram_data_v_lo[0])
+    ,.dram_data_ready_o()
+   );
 
 bsg_nonsynth_dramsim3
   #(.channel_addr_width_p(`dram_pkg::channel_addr_width_p)
@@ -127,8 +113,8 @@ bsg_nonsynth_dramsim3
     ,.init_mem_p(1)
    )
   dram
-   (.clk_i(clk_i)
-    ,.reset_i(reset_i)
+   (.clk_i(dram_clk)
+    ,.reset_i(dram_reset)
 
     ,.v_i(dram_v_li)
     ,.write_not_read_i(dram_w_li)
@@ -146,7 +132,22 @@ bsg_nonsynth_dramsim3
 
     ,.write_done_o()
     ,.write_done_ch_addr_o()
-    );
+   );
+
+bsg_nonsynth_clock_gen
+  #(.cycle_time_p(`dram_pkg::tck_ps))
+  clock_gen
+  (.o(dram_clk));
+
+bsg_nonsynth_reset_gen
+  #(.num_clocks_p(1)
+   ,.reset_cycles_lo_p(0)
+   ,.reset_cycles_hi_p(10)
+   )
+  reset_gen
+  (.clk_i(dram_clk)
+   ,.async_reset_o(dram_reset)
+  );
 
 endmodule
 
--- bp_me/test/common/bp_mem_to_dram.v
+++ bp_me/test/common/bp_mem_to_dram.v
@@ -6,11 +6,10 @@ module bp_mem_to_dram
   import bp_common_aviary_pkg::*;
   import bp_me_pkg::*;
   
-  import bsg_cache_pkg::*;
-  
  #(parameter bp_params_e bp_params_p = e_bp_inv_cfg
   `declare_bp_proc_params(bp_params_p)
   `declare_bp_me_if_widths(paddr_width_p, cce_block_width_p, lce_id_width_p, lce_assoc_p)
+  , parameter channel_addr_width_p = "inv"
   , parameter data_width_p = "inv"
   , parameter dram_base_p = "inv"
   , parameter fifo_els_p = "inv"
@@ -20,44 +19,46 @@ module bp_mem_to_dram
   , localparam byte_offset_width_lp =`BSG_SAFE_CLOG2(data_width_p>>3) 
   , localparam block_size_in_words_lp = cce_block_width_p / data_width_p
   , localparam lg_block_size_in_words_lp = `BSG_SAFE_CLOG2(block_size_in_words_lp)
-  , localparam bsg_cache_dma_pkt_width_lp = `bsg_cache_dma_pkt_width(paddr_width_p)
   , localparam reorder_fifo_els_lp = fifo_els_p * block_size_in_words_lp
   , localparam lg_reorder_fifo_els_lp = `BSG_SAFE_CLOG2(reorder_fifo_els_lp)
   )
   
-  (// Cache DMA side
+  (
+   // Core Side
    input                                           clk_i
   ,input                                           reset_i
-  // Sending address and write_en               
-  ,output       [bsg_cache_dma_pkt_width_lp-1:0]   dma_pkt_o
-  ,output                                          dma_pkt_v_o
-  ,input                                           dma_pkt_yumi_i
-  // Sending cache block                                          
-  ,output       [data_width_p-1:0]                 dma_data_o
-  ,output       [write_mask_width_lp-1:0]          dma_mask_o
-  ,output                                          dma_data_v_o
-  ,input                                           dma_data_yumi_i
-  // Receiving cache block                                        
-  ,input        [data_width_p-1:0]                 dma_data_i
-  ,input        [paddr_width_p-1:0]                dma_addr_i
-  ,input                                           dma_data_v_i
-  ,output                                          dma_data_ready_o
-  // Cmd input
+
   ,input        [cce_mem_msg_width_lp-1:0]         mem_cmd_i
   ,input                                           mem_cmd_v_i
   ,output                                          mem_cmd_ready_o
-  // Resp output
+
   ,output       [cce_mem_msg_width_lp-1:0]         mem_resp_o
   ,output                                          mem_resp_v_o
   ,input                                           mem_resp_yumi_i
+
+  // DRAM Side
+  ,input                                           dram_clk_i
+  ,input                                           dram_reset_i 
+
+  ,output       [channel_addr_width_p-1:0]         dram_ch_addr_o
+  ,output                                          dram_write_not_read_o
+  ,output                                          dram_v_o
+  ,input                                           dram_yumi_i
+
+  ,output       [data_width_p-1:0]                 dram_data_o
+  ,output       [write_mask_width_lp-1:0]          dram_mask_o
+  ,output                                          dram_data_v_o
+  ,input                                           dram_data_yumi_i
+
+  ,input        [data_width_p-1:0]                 dram_data_i
+  ,input        [channel_addr_width_p-1:0]         dram_ch_addr_i
+  ,input                                           dram_data_v_i
+  ,output                                          dram_data_ready_o
   );
 
   localparam fifo_width_lp = cce_mem_msg_width_lp - cce_block_width_p;
   
   /********************* Packet definition *********************/
-  
-  // Define cache DMA packet
-  `declare_bsg_cache_dma_pkt_s(paddr_width_p);
   
   // cce
   `declare_bp_me_if(paddr_width_p, cce_block_width_p, lce_id_width_p, lce_assoc_p);
@@ -66,173 +67,223 @@ module bp_mem_to_dram
   /********************* Resp queue fifo *********************/
   
   // Stores CCE packet header information
-  logic queue_fifo_valid_li, queue_fifo_ready_lo;
-  logic[fifo_width_lp-1:0] queue_fifo_data_li;
+  logic queue_fifo_v_li, queue_fifo_ready_lo;
+  logic [fifo_width_lp-1:0] queue_fifo_data_li;
   
-  logic queue_fifo_valid_lo, queue_fifo_yumi_li;
-  logic[fifo_width_lp-1:0] queue_fifo_data_lo;
+  logic queue_fifo_v_lo, queue_fifo_yumi_li;
+  logic [fifo_width_lp-1:0] queue_fifo_data_lo;
   
   bsg_fifo_1r1w_small
  #(.width_p(fifo_width_lp)
   ,.els_p  (fifo_els_p)
   ) queue_fifo
-  (.clk_i  (clk_i  )
+  (.clk_i  (clk_i)
   ,.reset_i(reset_i)
   ,.ready_o(queue_fifo_ready_lo)
-  ,.data_i (queue_fifo_data_li )
-  ,.v_i    (queue_fifo_valid_li)
-  ,.v_o    (queue_fifo_valid_lo)
-  ,.data_o (queue_fifo_data_lo )
-  ,.yumi_i (queue_fifo_yumi_li )
+  ,.data_i (queue_fifo_data_li)
+  ,.v_i    (queue_fifo_v_li)
+  ,.v_o    (queue_fifo_v_lo)
+  ,.data_o (queue_fifo_data_lo)
+  ,.yumi_i (queue_fifo_yumi_li)
   );
   
   
-  /********************* cce -> cache_dma *********************/
+  /********************* core -> dram *********************/
   
-  // dma pkt fifo
-  logic dma_pkt_fifo_valid_li, dma_pkt_fifo_ready_lo;
-  bsg_cache_dma_pkt_s dma_pkt_fifo_data_li;
-  logic[cce_write_mask_width_lp-1:0] mem_cmd_mask_li;
+  // Address Channel
+  logic dma_fifo_v_li, dma_fifo_ready_lo, dma_fifo_write_not_read_li;
+  logic [channel_addr_width_p-1:0] dma_fifo_ch_addr_li;
 
-  logic dma_pkt_fifo_valid_lo, dma_pkt_fifo_yumi_li;
-  bsg_cache_dma_pkt_s dma_pkt_fifo_data_lo;
-  logic[cce_write_mask_width_lp-1:0] mem_cmd_mask_lo;
- 
-  bsg_two_fifo
- #(.width_p(bsg_cache_dma_pkt_width_lp+cce_write_mask_width_lp)
-  ) dma_pkt_fifo
-  (.clk_i  (clk_i  )
-  ,.reset_i(reset_i)
-  ,.ready_o(dma_pkt_fifo_ready_lo)
-  ,.data_i ({mem_cmd_mask_li, dma_pkt_fifo_data_li})
-  ,.v_i    (dma_pkt_fifo_valid_li)
-  ,.v_o    (dma_pkt_fifo_valid_lo)
-  ,.data_o ({mem_cmd_mask_lo, dma_pkt_fifo_data_lo})
-  ,.yumi_i (dma_pkt_fifo_yumi_li )
-  );
+  logic dma_fifo_v_lo, dma_fifo_yumi_li, dma_fifo_write_not_read_lo;
+  logic [channel_addr_width_p-1:0] dma_fifo_ch_addr_lo;
+
+  logic req_afifo_enq_li, req_afifo_full_lo;
 
   logic [lg_block_size_in_words_lp-1:0] word_cnt_r;
+  logic [channel_addr_width_p-1:0] word_addr_lo;
+ 
+  bsg_two_fifo
+ #(.width_p(1+channel_addr_width_p)
+  ) dma_fifo
+  (.clk_i  (clk_i)
+  ,.reset_i(reset_i)
+  ,.ready_o(dma_fifo_ready_lo)
+  ,.data_i ({dma_fifo_write_not_read_li, dma_fifo_ch_addr_li})
+  ,.v_i    (dma_fifo_v_li)
+  ,.v_o    (dma_fifo_v_lo)
+  ,.data_o ({dma_fifo_write_not_read_lo, dma_fifo_ch_addr_lo})
+  ,.yumi_i (dma_fifo_yumi_li)
+  );
+
+  bsg_async_fifo
+  #(.lg_size_p(2)
+    ,.width_p(1+channel_addr_width_p)
+  ) req_afifo (
+    .w_clk_i(clk_i)
+    ,.w_reset_i(reset_i)
+    ,.w_enq_i(req_afifo_enq_li)
+    ,.w_data_i({dma_fifo_write_not_read_lo, word_addr_lo})
+    ,.w_full_o(req_afifo_full_lo)
+
+    ,.r_clk_i(dram_clk_i)
+    ,.r_reset_i(dram_reset_i)
+    ,.r_deq_i(dram_yumi_i)
+    ,.r_data_o({dram_write_not_read_o, dram_ch_addr_o})
+    ,.r_valid_o(dram_v_o)
+  );
+
   bsg_counter_clear_up
  #(.max_val_p(block_size_in_words_lp-1)
   ,.init_val_p(0)
   ) word_counter
   (.clk_i(clk_i)
   ,.reset_i(reset_i)
-  ,.clear_i(dma_pkt_fifo_yumi_li)
-  ,.up_i(dma_pkt_yumi_i & ~dma_pkt_fifo_yumi_li)
+  ,.clear_i(dma_fifo_yumi_li)
+  ,.up_i(req_afifo_enq_li & ~dma_fifo_yumi_li)
   ,.count_o(word_cnt_r)
   );
  
-  // dma data piso
-  logic dma_data_fifo_valid_li, dma_data_fifo_ready_lo;
-  logic [cce_block_width_p-1:0] dma_data_fifo_data_li;
-  
+  // Data Channel
+  logic dma_data_piso_v_li, dma_data_piso_ready_lo;
+  logic [cce_block_width_p-1:0] dma_data_piso_data_li;
+  logic [cce_write_mask_width_lp-1:0] dma_data_piso_mask_li;
+  logic dma_data_piso_v_lo, dma_data_piso_yumi_li;
+  logic [data_width_p-1:0] dma_data_piso_data_lo;
+  logic [write_mask_width_lp-1:0] dma_data_piso_mask_lo;
+
+  logic req_data_afifo_full_lo;
+
   bsg_parallel_in_serial_out 
  #(.width_p(data_width_p)
   ,.els_p  (block_size_in_words_lp)
   ) dma_data_piso
-  (.clk_i  (clk_i  )
+  (.clk_i  (clk_i)
   ,.reset_i(reset_i)
-  ,.valid_i(dma_data_fifo_valid_li)
-  ,.data_i (dma_data_fifo_data_li)
-  ,.ready_o(dma_data_fifo_ready_lo)
-  ,.valid_o(dma_data_v_o)
-  ,.data_o (dma_data_o)
-  ,.yumi_i (dma_data_yumi_i)
+  ,.valid_i(dma_data_piso_v_li)
+  ,.data_i (dma_data_piso_data_li)
+  ,.ready_o(dma_data_piso_ready_lo)
+  ,.valid_o(dma_data_piso_v_lo)
+  ,.data_o (dma_data_piso_data_lo)
+  ,.yumi_i (dma_data_piso_yumi_li)
+  );
+
+  bsg_parallel_in_serial_out
+ #(.width_p(write_mask_width_lp)
+  ,.els_p  (block_size_in_words_lp)
+  ) dma_mask_piso
+  (.clk_i  (clk_i)
+  ,.reset_i(reset_i)
+  ,.valid_i(dma_data_piso_v_li)
+  ,.data_i (dma_data_piso_mask_li)
+  ,.ready_o()
+  ,.valid_o()
+  ,.data_o (dma_data_piso_mask_lo)
+  ,.yumi_i (dma_data_piso_yumi_li)
+  );
+
+  bsg_async_fifo
+  #(.lg_size_p(2)
+    ,.width_p(write_mask_width_lp+data_width_p)
+  ) req_data_afifo (
+    .w_clk_i(clk_i)
+    ,.w_reset_i(reset_i)
+    ,.w_enq_i(dma_data_piso_yumi_li)
+    ,.w_data_i({dma_data_piso_mask_lo, dma_data_piso_data_lo})
+    ,.w_full_o(req_data_afifo_full_lo)
+
+    ,.r_clk_i(dram_clk_i)
+    ,.r_reset_i(dram_reset_i)
+    ,.r_deq_i(dram_data_yumi_i)
+    ,.r_data_o({dram_mask_o, dram_data_o})
+    ,.r_valid_o(dram_data_v_o)
   );
 
   // input mem cmd
-  bp_cce_mem_msg_s mem_cmd_li;
-  
+  bp_cce_mem_msg_s mem_cmd_li , mem_resp_lo;
   assign mem_cmd_li = mem_cmd_i;
-  assign mem_cmd_ready_o = queue_fifo_ready_lo & dma_pkt_fifo_ready_lo & dma_data_fifo_ready_lo;
+  assign mem_resp_o = mem_resp_lo;
+  assign mem_cmd_ready_o = ~reset_i & ~dram_reset_i & queue_fifo_ready_lo & dma_fifo_ready_lo & dma_data_piso_ready_lo;
 
-  bsg_cache_dma_pkt_s dma_pkt_lo;
+  // cmd queue
+  assign queue_fifo_v_li = mem_cmd_v_i & mem_cmd_ready_o;
+  assign queue_fifo_data_li = mem_cmd_li.header;
 
-  assign dma_pkt_v_o = dma_pkt_fifo_valid_lo;
-  assign dma_pkt_o = dma_pkt_lo;
+  // address channel
+  assign dma_fifo_v_li = mem_cmd_v_i & mem_cmd_ready_o;
+  assign dma_fifo_yumi_li =  req_afifo_enq_li & (word_cnt_r == (block_size_in_words_lp-1));
+  assign dma_fifo_write_not_read_li = (mem_cmd_li.header.msg_type inside {e_cce_mem_uc_wr, e_cce_mem_wr});
+  assign dma_fifo_ch_addr_li = mem_cmd_li.header.addr - dram_base_p;
+  assign req_afifo_enq_li = dma_fifo_v_lo & ~req_afifo_full_lo;
+  assign word_addr_lo = dma_fifo_ch_addr_lo + (word_cnt_r << byte_offset_width_lp);
 
-  assign dma_pkt_fifo_yumi_li = dma_pkt_yumi_i & (word_cnt_r == (block_size_in_words_lp-1));
-  assign dma_pkt_lo.write_not_read = dma_pkt_fifo_data_lo.write_not_read;
-  assign dma_pkt_lo.addr = dma_pkt_fifo_data_lo.addr + (word_cnt_r << byte_offset_width_lp) - dram_base_p;
-  
-  assign dma_mask_o = mem_cmd_mask_lo >> cce_write_mask_width_lp'(word_cnt_r << byte_offset_width_lp);
+  // data channel
+  assign dma_data_piso_v_li = mem_cmd_v_i & mem_cmd_ready_o & dma_fifo_write_not_read_li;
+  assign dma_data_piso_yumi_li = dma_data_piso_v_lo & ~req_data_afifo_full_lo;
+  assign dma_data_piso_data_li = mem_cmd_li.data << cce_block_width_p'(mem_cmd_li.header.addr[0+:byte_offset_width_lp] << 3);
+  assign dma_data_piso_mask_li = ((1 << (1 << mem_cmd_li.header.size)) - 1) << mem_cmd_li.header.addr[0+:byte_offset_width_lp];
 
-  // combinational logics
-  always_comb 
-  begin
-  
-    dma_pkt_fifo_valid_li = 1'b0;
-    dma_data_fifo_valid_li = 1'b0;
-    queue_fifo_valid_li = 1'b0;
-    
-    dma_pkt_fifo_data_li.write_not_read = (mem_cmd_li.header.msg_type inside {e_cce_mem_uc_wr, e_cce_mem_wr});
-    dma_pkt_fifo_data_li.addr = mem_cmd_li.header.addr;
-    mem_cmd_mask_li = ((1 << (1 << mem_cmd_li.header.size)) - 1) << mem_cmd_li.header.addr[0+:byte_offset_width_lp];
-    dma_data_fifo_data_li = mem_cmd_li.data << cce_block_width_p'(mem_cmd_li.header.addr[0+:byte_offset_width_lp] << 3);
-    queue_fifo_data_li = mem_cmd_li.header;
+  /********************* dram -> core *********************/
 
-    if (mem_cmd_v_i & dma_pkt_fifo_ready_lo & queue_fifo_ready_lo)
-      begin
-        if (dma_pkt_fifo_data_li.write_not_read)
-          begin
-            if (dma_data_fifo_ready_lo)
-              begin
-                dma_pkt_fifo_valid_li = 1'b1;
-                queue_fifo_valid_li = 1'b1;
-                dma_data_fifo_valid_li = 1'b1;
-              end
-          end
-        else
-          begin
-            dma_pkt_fifo_valid_li = 1'b1;
-            queue_fifo_valid_li = 1'b1;
-          end
-      end
-  
-  end
-  
-  
-  /********************* cache_dma -> cce *********************/
+  // dma data sipo
+  logic dma_data_sipo_v_lo, dma_data_sipo_yumi_li, dma_data_sipo_ready_lo;
+  logic [cce_block_width_p-1:0] dma_data_sipo_data_lo;
 
-  // dma data sipof
-  logic dma_data_fifo_valid_lo, dma_data_fifo_yumi_li,
-  dma_data_sipo_ready_lo;
-  logic [cce_block_width_p-1:0] dma_data_fifo_data_lo;
+  // resp async FIFO
+  logic resp_afifo_v_lo, resp_afifo_deq_li, resp_afifo_full_lo;
+  logic [data_width_p-1:0] resp_afifo_data_lo;
+  logic [channel_addr_width_p-1:0] resp_afifo_ch_addr_lo;
 
   // data reorder FIFO
   logic reorder_alloc_v_lo, reorder_alloc_yumi_li;
   logic [lg_reorder_fifo_els_lp-1:0] reorder_alloc_id_lo;
 
-  logic reorder_w_v_li;
-  logic [lg_reorder_fifo_els_lp-1:0] reorder_w_id_li;
-  logic [data_width_p-1:0] reorder_w_data_li;
-
   logic reorder_deq_v_lo, reorder_deq_yumi_li;
   logic [data_width_p-1:0] reorder_deq_data_lo;
 
   // reordering CAM
-  logic cam_w_set_not_clear_i, cam_r_v_li;
+  logic cam_w_set_not_clear_i;
   logic [reorder_fifo_els_lp-1:0] cam_w_v_li, cam_r_match_lo;
-  logic [paddr_width_p-1:0] cam_w_tag_li, cam_r_tag_li;
+  logic [channel_addr_width_p-1:0] cam_w_tag_li;
 
   // id decoder
   logic [reorder_fifo_els_lp-1:0] id_decode_lo;
 
-  bsg_serial_in_parallel_out_full
- #(.width_p(data_width_p)
-  ,.els_p  (block_size_in_words_lp)
-  ) dma_data_sipof
-  (.clk_i  (clk_i  )
-  ,.reset_i(reset_i)
-  
-  ,.v_i    (reorder_deq_v_lo)
-  ,.ready_o(dma_data_sipo_ready_lo)
-  ,.data_i (reorder_deq_data_lo)
+  // id encoder
+  logic id_encode_v_lo;
+  logic [lg_reorder_fifo_els_lp-1:0] id_encode_lo;
 
-  ,.data_o (dma_data_fifo_data_lo )
-  ,.v_o    (dma_data_fifo_valid_lo)
-  ,.yumi_i (dma_data_fifo_yumi_li)
+  bsg_async_fifo
+  #(.lg_size_p(2)
+    ,.width_p(channel_addr_width_p+data_width_p)
+  ) resp_afifo (
+    .w_clk_i(dram_clk_i)
+    ,.w_reset_i(dram_reset_i)
+    ,.w_enq_i(dram_data_v_i)
+    ,.w_data_i({dram_ch_addr_i, dram_data_i})
+    ,.w_full_o(resp_afifo_full_lo)
+
+    ,.r_clk_i(clk_i)
+    ,.r_reset_i(reset_i)
+    ,.r_deq_i(resp_afifo_deq_li)
+    ,.r_data_o({resp_afifo_ch_addr_lo, resp_afifo_data_lo})
+    ,.r_valid_o(resp_afifo_v_lo)
+  );
+
+  bsg_cam_1r1w_tag_array
+ #(.els_p(reorder_fifo_els_lp)
+  ,.width_p(channel_addr_width_p)
+  ) id_cam
+  (.clk_i(clk_i)
+  ,.reset_i(reset_i)
+
+  ,.w_v_i(cam_w_v_li)
+  ,.w_set_not_clear_i(cam_w_set_not_clear_i)
+  ,.w_tag_i(cam_w_tag_li)
+  ,.w_empty_o()
+
+  ,.r_v_i(resp_afifo_v_lo)
+  ,.r_tag_i(resp_afifo_ch_addr_lo)
+  ,.r_match_o(cam_r_match_lo)
   );
 
   bsg_fifo_reorder
@@ -246,9 +297,9 @@ module bp_mem_to_dram
   ,.fifo_alloc_id_o(reorder_alloc_id_lo)
   ,.fifo_alloc_yumi_i(reorder_alloc_yumi_li)
 
-  ,.write_v_i(reorder_w_v_li)
-  ,.write_id_i(reorder_w_id_li)
-  ,.write_data_i(reorder_w_data_li)
+  ,.write_v_i(id_encode_v_lo)
+  ,.write_id_i(id_encode_lo)
+  ,.write_data_i(resp_afifo_data_lo)
 
   ,.fifo_deq_v_o(reorder_deq_v_lo)
   ,.fifo_deq_data_o(reorder_deq_data_lo)
@@ -257,29 +308,28 @@ module bp_mem_to_dram
   ,.empty_o()
   );
 
-  bsg_cam_1r1w_tag_array
- #(.els_p(reorder_fifo_els_lp)
-  ,.width_p(paddr_width_p)
-  ) id_cam
-  (.clk_i(clk_i)
+  bsg_serial_in_parallel_out_full
+ #(.width_p(data_width_p)
+  ,.els_p  (block_size_in_words_lp)
+  ) dma_data_sipo
+  (.clk_i  (clk_i)
   ,.reset_i(reset_i)
+  
+  ,.v_i    (reorder_deq_v_lo)
+  ,.ready_o(dma_data_sipo_ready_lo)
+  ,.data_i (reorder_deq_data_lo)
 
-  ,.w_v_i(cam_w_v_li)
-  ,.w_set_not_clear_i(cam_w_set_not_clear_i)
-  ,.w_tag_i(cam_w_tag_li)
-  ,.w_empty_o()
-
-  ,.r_v_i(cam_r_v_li)
-  ,.r_tag_i(cam_r_tag_li)
-  ,.r_match_o(cam_r_match_lo)
+  ,.data_o (dma_data_sipo_data_lo)
+  ,.v_o    (dma_data_sipo_v_lo)
+  ,.yumi_i (dma_data_sipo_yumi_li)
   );
 
   bsg_encode_one_hot
  #(.width_p(reorder_fifo_els_lp)
   ) id_enc
   (.i(cam_r_match_lo)
-  ,.addr_o(reorder_w_id_li)
-  ,.v_o(reorder_w_v_li)
+  ,.addr_o(id_encode_lo)
+  ,.v_o(id_encode_v_lo)
   );
 
   bsg_decode
@@ -289,67 +339,36 @@ module bp_mem_to_dram
   ,.o(id_decode_lo)
   );
 
-  // Reordering logic
+  // queue FIFO
+  wire is_write = mem_resp_lo.header.msg_type inside {e_cce_mem_uc_wr, e_cce_mem_wr};
+  assign queue_fifo_yumi_li = mem_resp_v_o & mem_resp_yumi_i;
+
+  // resp async FIFO
+  // Cannot accept data when sending read requests
+  assign resp_afifo_deq_li = resp_afifo_v_lo & ~reorder_alloc_yumi_li;
+
+  // ID -> address CAM
   assign cam_w_v_li = reorder_alloc_yumi_li
                       ? id_decode_lo
-                      : dma_data_v_i
+                      : resp_afifo_v_lo
                         ? cam_r_match_lo
                         : '0;
   assign cam_w_set_not_clear_i = reorder_alloc_yumi_li;
-  assign cam_w_tag_li = dma_pkt_lo.addr;
-  assign cam_r_v_li = dma_data_v_i;
-  assign cam_r_tag_li = dma_addr_i;
+  assign cam_w_tag_li = word_addr_lo;
 
-  assign reorder_alloc_yumi_li = dma_pkt_v_o & ~dma_pkt_lo.write_not_read;
-  assign reorder_w_data_li = dma_data_i;
-  assign reorder_deq_yumi_li = dma_data_sipo_ready_lo & reorder_deq_v_lo;
+  // reorder FIFO
+  assign reorder_alloc_yumi_li = req_afifo_enq_li & ~dma_fifo_write_not_read_lo;
+  assign reorder_deq_yumi_li = reorder_deq_v_lo & dma_data_sipo_ready_lo;
 
-  // Cannot accept data when sendign read requests
-  assign dma_data_ready_o = ~reorder_alloc_yumi_li;
- 
+  // data sipo
+  assign dma_data_sipo_yumi_li = queue_fifo_yumi_li & ~is_write;
+
   // mem resp output
-  bp_cce_mem_msg_s mem_resp_lo;
-  logic mem_resp_v_lo;
-  
-  assign mem_resp_o = mem_resp_lo;
-  assign mem_resp_v_o = mem_resp_v_lo;
-  
-  // combinational logics
-  always_comb
-  begin
-  
-    mem_resp_v_lo = 1'b0;
-    dma_data_fifo_yumi_li = 1'b0;
-    queue_fifo_yumi_li = 1'b0;
-    
-    mem_resp_lo.header = queue_fifo_data_lo;
-    mem_resp_lo.data = dma_data_fifo_data_lo;
-    
-    if (~reset_i & queue_fifo_valid_lo)
-      begin
-        if (mem_resp_lo.header.msg_type inside {e_cce_mem_uc_wr, e_cce_mem_wr})
-          begin
-            mem_resp_lo.data = '0;
-            mem_resp_v_lo = 1'b1;
-            if (mem_resp_yumi_i)
-              begin
-                queue_fifo_yumi_li = 1'b1;
-              end
-          end
-        else
-          begin
-            if (dma_data_fifo_valid_lo)
-              begin
-                mem_resp_v_lo = 1'b1;
-                if (mem_resp_yumi_i)
-                  begin
-                    queue_fifo_yumi_li = 1'b1;
-                    dma_data_fifo_yumi_li = 1'b1;
-                  end
-              end
-          end
-      end
-    
-  end
-  
+  assign mem_resp_lo.header = queue_fifo_data_lo;
+  assign mem_resp_lo.data = is_write ? '0 : dma_data_sipo_data_lo;
+  assign mem_resp_v_o = queue_fifo_v_lo & (is_write | dma_data_sipo_v_lo);
+
+  // resp data
+  assign dram_data_ready_o = ~reset_i & ~dram_reset_i & ~resp_afifo_full_lo;
+
 endmodule
