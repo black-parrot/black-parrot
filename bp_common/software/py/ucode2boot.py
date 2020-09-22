0abda5696bbe9cf1565192562fa224df1b7838b3
sripathi-muralitharan <sripathimuralitharan@gmail.com>
2020-09-22 13:54:12 -0700

    Python script to convert ucode to bin format to include in bootrom

---
+++ bp_common/software/py/ucode2boot.py
@@ -0,0 +1,21 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--ucode', dest='ucode_file', type=str, required=True, help='CCE ucode file')
+parser.add_argument('--path', dest='path', type=str, required=True, help='Output path')
+args = parser.parse_args()
+
+out_file = args.path + "/cce_ucode.mem"
+wf = open(out_file, 'w')
+with open(ucode_file, 'r') as rf:
+  lines = rf.readlines()
+  for line in lines:
+    line = line.strip()
+    hex64 = hex(int(line.zfill(64), 2))
+    wf.write(str(hex64)[2:].zfill(16))
+    wf.write("\n")
+  wf.write("FFFFFFF000000000")
+  wf.write("\n")
+
+rf.close()
+wf.close()
