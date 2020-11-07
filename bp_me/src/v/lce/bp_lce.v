5fb17ed84d6031509afab3c1d47e22d97ca1d6cc
Dan Petrisko <petrisko@cs.washington.edu>
2020-11-06 17:00:33 -0800

    Fixing up synchronization sequence in LCEs

--- bp_me/src/v/lce/bp_lce.v
+++ bp_me/src/v/lce/bp_lce.v
@@ -125,6 +125,7 @@ module bp_lce
   // LCE Request Module
   logic req_ready_lo;
   logic uc_store_req_complete_lo;
+  logic sync_done_lo;
   bp_lce_req
     #(.bp_params_p(bp_params_p)
       ,.assoc_p(assoc_p)
@@ -140,6 +141,7 @@ module bp_lce
 
       ,.lce_id_i(lce_id_i)
       ,.lce_mode_i(lce_mode_i)
+      ,.sync_done_i(sync_done_lo)
 
       ,.ready_o(req_ready_lo)
 
@@ -161,7 +163,6 @@ module bp_lce
 
   // LCE Command Module
   logic cmd_ready_lo;
-  logic cmd_sync_done_lo;
   bp_lce_cmd
     #(.bp_params_p(bp_params_p)
       ,.assoc_p(assoc_p)
@@ -180,7 +181,7 @@ module bp_lce
       ,.lce_mode_i(lce_mode_i)
 
       ,.ready_o(cmd_ready_lo)
-      ,.sync_done_o(cmd_sync_done_lo)
+      ,.sync_done_o(sync_done_lo)
       ,.cache_req_complete_o(cache_req_complete_o)
       ,.cache_req_critical_o(cache_req_critical_o)
       ,.uc_store_req_complete_o(uc_store_req_complete_lo)
