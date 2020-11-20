aa73e250665e182a95ffc8d45a40dcfc122e64d8
Dan Petrisko <petrisko@cs.washington.edu>
2020-11-20 14:57:02 -0800

    Adding assertion for illegal $ configuration

--- bp_be/src/v/bp_be_mem/bp_be_dcache/bp_be_dcache.v
+++ bp_be/src/v/bp_be_mem/bp_be_dcache/bp_be_dcache.v
@@ -1286,6 +1286,10 @@ module bp_be_dcache
   if ( (dcache_block_width_p/dcache_assoc_p) < dword_width_p || dcache_fill_width_p > dcache_block_width_p ) begin // dcache parameter check in compile time
     $error("ERROR: The dcache supports multi-cycle fill/eviction with the following constraints: - bank_width = block_width / assoc >= dword_width and fill_width = N*bank_width <= block_width. Please adjust the parameters according to these conditions and recompile");
   end
+
+  if (`BSG_SAFE_CLOG2(dcache_block_width_p*dcache_sets_p/8) != page_offset_width_p) begin
+    $error("Total cache size must be equal to 4kB * associativity");
+  end
  
   always_ff @ (posedge clk_i) begin
     if (v_tv_r) begin
