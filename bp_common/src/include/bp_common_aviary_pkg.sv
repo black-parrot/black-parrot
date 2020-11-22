ca36b253aa2ad697413e03c0197be96801797928
Dan Petrisko <petrisko@cs.washington.edu>
2020-11-21 22:23:59 -0800

    Reducing tag width

--- bp_common/src/include/bp_common_aviary_pkg.sv
+++ bp_common/src/include/bp_common_aviary_pkg.sv
@@ -30,7 +30,7 @@ package bp_common_aviary_pkg;
       ,boot_in_debug : 0
 
       ,branch_metadata_fwd_width: 37
-      ,btb_tag_width            : 10
+      ,btb_tag_width            : 9
       ,btb_idx_width            : 6
       ,bht_idx_width            : 9
       ,ghist_width              : 2
