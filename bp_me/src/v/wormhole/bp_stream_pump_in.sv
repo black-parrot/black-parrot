c689860ccfa0835c95deb1d19783ca69f74f3dd9
Songchun <rafael.fc.lee@outlook.com>
2020-12-02 13:44:33 -0800

    Removing unused localparam in stream pump

--- bp_me/src/v/wormhole/bp_stream_pump_in.sv
+++ bp_me/src/v/wormhole/bp_stream_pump_in.sv
@@ -21,7 +21,6 @@ module bp_stream_pump_in
    , localparam stream_words_lp = block_width_p / stream_data_width_p
    , localparam data_len_width_lp = `BSG_SAFE_CLOG2(stream_words_lp)
    , localparam stream_offset_width_lp = `BSG_SAFE_CLOG2(stream_data_width_p / 8)
-   , localparam logic resp_payload_lp  = payload_mask_p[e_bedrock_mem_uc_rd] | payload_mask_p[e_bedrock_mem_rd]
    )
   ( input clk_i
   , input reset_i
