98b86ca78b0e88f5fd05c9156b1e2081f8a75b5e
Songchun <rafael.fc.lee@outlook.com>
2021-03-31 23:26:04 -0700

    Updating parameter restriction in if_verif

--- bp_top/test/common/bp_nonsynth_if_verif.sv
+++ bp_top/test/common/bp_nonsynth_if_verif.sv
@@ -74,11 +74,13 @@ module bp_nonsynth_if_verif
     $fatal("Error: Cache fill width should be less or equal to L1 cache block width");
   if ((icache_fill_width_p % (icache_block_width_p/icache_assoc_p) != 0) || (dcache_fill_width_p % (dcache_block_width_p / dcache_assoc_p) != 0))
     $fatal("Error: Cache fill width should be a multiple of cache bank width");
+  if (icache_fill_width_p != dcache_fill_width_p)
+    $fatal("Error: L1-Cache fill width should be the same");
 
   if (l2_block_width_p != 512)
     $error("L2 block width must be 512");
-  if (l2_data_width_p != 64)
-    $error("L2 data width must be 64");
+  if ((l2_data_width_p < icache_fill_width_p) || (l2_data_width_p < dcache_fill_width_p))
+    $error("L2 data width must larger than l1 fill width");
 
   if (vaddr_width_p != 39)
     $warning("Warning: VM will not work without 39 bit vaddr");
