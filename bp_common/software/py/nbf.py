198e2fb72242189d614ffa5f9d0126d9427945e8
Dan Petrisko <petrisko@cs.washington.edu>
2021-05-21 07:39:35 -0700

    Adding validate flag to NBF

--- bp_common/software/py/nbf.py
+++ bp_common/software/py/nbf.py
@@ -237,9 +237,10 @@ class NBF:
       self.print_nbf_allcores(3, cfg_base_addr + cfg_reg_cce_mode, 1)
 
       # Read back I$, D$ and CCE modes for verification
-      self.print_nbf(0x12, cfg_base_addr + cfg_reg_icache_mode, 1)
-      self.print_nbf(0x12, cfg_base_addr + cfg_reg_dcache_mode, 1)
-      self.print_nbf(0x12, cfg_base_addr + cfg_reg_cce_mode, 1)
+      if self.validate:
+        self.print_nbf(0x12, cfg_base_addr + cfg_reg_icache_mode, 1)
+        self.print_nbf(0x12, cfg_base_addr + cfg_reg_dcache_mode, 1)
+        self.print_nbf(0x12, cfg_base_addr + cfg_reg_cce_mode, 1)
 
     self.print_fence()
 
@@ -276,6 +277,7 @@ if __name__ == "__main__":
   parser.add_argument('--skip_zeros', dest='skip_zeros', action='store_true', help='skip zero DRAM entries')
   parser.add_argument('--addr_width', type=int, default=40, help='Physical address width')
   parser.add_argument('--data_width', type=int, default=64, help='Data width')
+  parser.add_argument('--validate', dest='validate', action='store_true', help='Data width')
 
   args = parser.parse_args()
 
