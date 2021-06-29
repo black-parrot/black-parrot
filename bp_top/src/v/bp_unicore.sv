6d4893a2e0cd44e87ac468c115e1301ee90f1bad
Dan Petrisko <petrisko@cs.washington.edu>
2021-06-28 22:59:42 -0700

    Updating unicore interfaces to stream

--- bp_top/src/v/bp_unicore.sv
+++ bp_top/src/v/bp_unicore.sv
@@ -21,22 +21,30 @@ module bp_unicore
    , input                                             reset_i
 
    // Outgoing I/O
-   , output logic [uce_mem_msg_width_lp-1:0]           io_cmd_o
+   , output logic [uce_mem_msg_header_width_lp-1:0]    io_cmd_header_o
+   , output logic [uce_mem_data_width_lp-1:0]          io_cmd_data_o
    , output logic                                      io_cmd_v_o
    , input                                             io_cmd_ready_and_i
+   , output logic                                      io_cmd_last_o
 
-   , input [uce_mem_msg_width_lp-1:0]                  io_resp_i
+   , input [uce_mem_msg_header_width_lp-1:0]           io_resp_header_i
+   , input [uce_mem_data_width_lp-1:0]                 io_resp_data_i
    , input                                             io_resp_v_i
    , output logic                                      io_resp_yumi_o
+   , input                                             io_resp_last_i
 
    // Incoming I/O
-   , input [uce_mem_msg_width_lp-1:0]                  io_cmd_i
+   , input [uce_mem_msg_header_width_lp-1:0]           io_cmd_header_i
+   , input [uce_mem_data_width_lp-1:0]                 io_cmd_data_i
    , input                                             io_cmd_v_i
    , output logic                                      io_cmd_yumi_o
+   , input                                             io_cmd_last_i
 
-   , output logic [uce_mem_msg_width_lp-1:0]           io_resp_o
+   , output logic [uce_mem_msg_header_width_lp-1:0]    io_resp_header_o
+   , output logic [uce_mem_data_width_lp-1:0]          io_resp_data_o
    , output logic                                      io_resp_v_o
    , input                                             io_resp_ready_and_i
+   , output logic                                      io_resp_last_o
 
    // DRAM interface
    , output logic [dma_pkt_width_lp-1:0]               dma_pkt_o
@@ -57,10 +65,12 @@ module bp_unicore
   `declare_bp_cache_engine_if(paddr_width_p, ctag_width_p, icache_sets_p, icache_assoc_p, dword_width_gp, icache_block_width_p, icache_fill_width_p, icache);
   `declare_bp_bedrock_mem_if(paddr_width_p, uce_mem_data_width_lp, lce_id_width_p, lce_assoc_p, uce);
 
-  bp_bedrock_uce_mem_msg_s mem_cmd_lo;
-  logic mem_cmd_v_lo, mem_cmd_ready_and_li;
-  bp_bedrock_uce_mem_msg_s mem_resp_li;
-  logic mem_resp_v_li, mem_resp_yumi_lo;
+  bp_bedrock_uce_mem_msg_header_s mem_cmd_header_lo;
+  logic [uce_mem_data_width_lp-1:0] mem_cmd_data_lo;
+  logic mem_cmd_v_lo, mem_cmd_ready_and_li, mem_resp_last_lo;
+  bp_bedrock_uce_mem_msg_header_s mem_resp_header_li;
+  logic [uce_mem_data_width_lp-1:0] mem_resp_data_li;
+  logic mem_resp_v_li, mem_resp_yumi_lo, mem_resp_last_li;
 
   bp_unicore_lite
    #(.bp_params_p(bp_params_p))
@@ -68,29 +78,20 @@ module bp_unicore
     (.clk_i(clk_i)
      ,.reset_i(reset_i)
 
-     ,.io_cmd_o(io_cmd_o)
-     ,.io_cmd_v_o(io_cmd_v_o)
-     ,.io_cmd_ready_and_i(io_cmd_ready_and_i)
-
-     ,.io_resp_i(io_resp_i)
-     ,.io_resp_v_i(io_resp_v_i)
-     ,.io_resp_yumi_o(io_resp_yumi_o)
-
-     ,.io_cmd_i(io_cmd_i)
-     ,.io_cmd_v_i(io_cmd_v_i)
-     ,.io_cmd_yumi_o(io_cmd_yumi_o)
-
-     ,.io_resp_o(io_resp_o)
-     ,.io_resp_v_o(io_resp_v_o)
-     ,.io_resp_ready_and_i(io_resp_ready_and_i)
-
-     ,.mem_cmd_o(mem_cmd_lo)
+     ,.mem_cmd_header_o(mem_cmd_header_lo)
+     ,.mem_cmd_data_o(mem_cmd_data_lo)
      ,.mem_cmd_v_o(mem_cmd_v_lo)
      ,.mem_cmd_ready_and_i(mem_cmd_ready_and_li)
+     ,.mem_cmd_last_o(mem_cmd_last_lo)
 
-     ,.mem_resp_i(mem_resp_li)
+     ,.mem_resp_header_i(mem_resp_header_li)
+     ,.mem_resp_data_i(mem_resp_data_li)
      ,.mem_resp_v_i(mem_resp_v_li)
      ,.mem_resp_yumi_o(mem_resp_yumi_lo)
+     ,.mem_resp_last_i(mem_resp_last_li)
+
+     // I/O
+     ,.*
      );
 
   import bsg_cache_pkg::*;
@@ -105,11 +106,12 @@ module bp_unicore
     (.clk_i(clk_i)
      ,.reset_i(reset_i)
 
-     ,.mem_cmd_i(mem_cmd_lo)
+     // TODO: Make stream
+     ,.mem_cmd_i({mem_cmd_data_lo, mem_cmd_header_lo})
      ,.mem_cmd_v_i(mem_cmd_v_lo)
      ,.mem_cmd_ready_and_o(mem_cmd_ready_and_li)
 
-     ,.mem_resp_o(mem_resp_li)
+     ,.mem_resp_o({mem_resp_data_li, mem_resp_header_li})
      ,.mem_resp_v_o(mem_resp_v_li)
      ,.mem_resp_yumi_i(mem_resp_yumi_lo)
 
