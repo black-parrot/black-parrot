a36032c568583b94050021c3034210fafdadd196
Mark Wyse <muwyse@gmail.com>
2021-07-08 16:06:31 -0700

    me: update burst-stream converters

--- bp_me/src/v/network/bp_burst_to_stream.sv
+++ bp_me/src/v/network/bp_burst_to_stream.sv
@@ -1,3 +1,16 @@
+/**
+ *
+ * Name:
+ *   bp_burst_to_stream.sv
+ *
+ * Description:
+ *   Converts BedRock Burst to Stream
+ *
+ *   BedRock Burst input implementation is minimal and accepts header before accepting data.
+ *   Data is not accepted in same cycle as header to avoid a dependence between in_msg_header_v_i
+ *   and in_msg_data_ready_and_o.
+ *
+ */
 
 `include "bp_common_defines.svh"
 `include "bp_me_defines.svh"
@@ -7,60 +20,52 @@ module bp_burst_to_stream
  import bp_me_pkg::*;
  #(parameter bp_params_e bp_params_p = e_bp_default_cfg
    `declare_bp_proc_params(bp_params_p)
-   // Assuming in_data_width_p(burst) == out_data_width_p(stream)
-   , parameter in_data_width_p  = "inv"
-   , parameter out_data_width_p = in_data_width_p
-   , parameter block_width_p = "inv"
-   , parameter payload_width_p  = "inv"
+   , parameter data_width_p = dword_width_gp
+   , parameter payload_width_p = "inv"
+   , parameter block_width_p = cce_block_width_p
 
    // Bitmask which determines which message types have a data payload
    // Constructed as (1 << e_payload_msg1 | 1 << e_payload_msg2)
    , parameter payload_mask_p = 0
 
-   `declare_bp_bedrock_if_widths(paddr_width_p, payload_width_p, in_data_width_p, lce_id_width_p, lce_assoc_p, in)
-   `declare_bp_bedrock_if_widths(paddr_width_p, payload_width_p, out_data_width_p, lce_id_width_p, lce_assoc_p, out)
-
+   `declare_bp_bedrock_if_widths(paddr_width_p, payload_width_p, data_width_p, lce_id_width_p, lce_assoc_p, bp)
    )
   (input                                            clk_i
    , input                                          reset_i
 
-   // Master BP Burst
+   // Input BedRock Burst
    // ready-valid-and
-   , input [in_msg_header_width_lp-1:0]      in_msg_header_i
-   , input                                   in_msg_header_v_i
-   , output logic                            in_msg_header_ready_and_o
+   , input [bp_msg_header_width_lp-1:0]             in_msg_header_i
+   , input                                          in_msg_header_v_i
+   , input                                          in_msg_has_data_i
+   , output logic                                   in_msg_header_ready_and_o
 
    // ready-valid-and
-   , input [in_data_width_p-1:0]             in_msg_data_i
-   , input                                   in_msg_data_v_i
-   , output logic                            in_msg_data_ready_and_o
+   , input [data_width_p-1:0]                       in_msg_data_i
+   , input                                          in_msg_data_v_i
+   , input                                          in_msg_last_i
+   , output logic                                   in_msg_data_ready_and_o
 
-   // Client BP Stream
+   // Output BedRock Stream
    // ready-valid-and
-   , output logic [out_msg_header_width_lp-1:0]     out_msg_header_o
-   , output logic [out_data_width_p-1:0]            out_msg_data_o
+   , output logic [bp_msg_header_width_lp-1:0]      out_msg_header_o
+   , output logic [data_width_p-1:0]                out_msg_data_o
    , output logic                                   out_msg_v_o
    , input                                          out_msg_ready_and_i
    , output logic                                   out_msg_last_o
    );
 
-  `declare_bp_bedrock_if(paddr_width_p, payload_width_p, in_data_width_p, lce_id_width_p, lce_assoc_p, in);
-  `declare_bp_bedrock_if(paddr_width_p, payload_width_p, out_data_width_p, lce_id_width_p, lce_assoc_p, out);
+  `declare_bp_bedrock_if(paddr_width_p, payload_width_p, data_width_p, lce_id_width_p, lce_assoc_p, bp);
 
-  localparam in_data_bytes_lp = in_data_width_p/8;
-  localparam out_data_bytes_lp = out_data_width_p/8;
-  localparam stream_words_lp = block_width_p/out_data_width_p;
-  localparam data_len_width_lp = (stream_words_lp>0) ? `BSG_SAFE_CLOG2(stream_words_lp) : 0;
-  localparam stream_offset_width_lp = `BSG_SAFE_CLOG2(out_data_width_p / 8);
-
-  bp_bedrock_out_msg_header_s out_header_lo;
+  bp_bedrock_bp_msg_header_s msg_header_li;
+  logic msg_has_data_li;
   bsg_dff_en_bypass
-   #(.width_p($bits(bp_bedrock_in_msg_header_s)))
+   #(.width_p($bits(bp_bedrock_bp_msg_header_s)+1))
    header_reg
     (.clk_i(clk_i)
     ,.en_i(in_msg_header_ready_and_o & in_msg_header_v_i)
-    ,.data_i(in_msg_header_i)
-    ,.data_o(out_header_lo)
+    ,.data_i({in_msg_has_data_i, in_msg_header_i})
+    ,.data_o({msg_has_data_li, msg_header_li})
     );
 
   logic header_v_r, header_clear, header_v_lo;
@@ -76,87 +81,41 @@ module bp_burst_to_stream
     );
   assign header_v_lo  = in_msg_header_v_i | header_v_r;
   assign header_clear = out_msg_ready_and_i & out_msg_v_o & out_msg_last_o;
-
-  // Accept no new header as long as a valid header exists
+  // Accept new header only if there is no header currently buffered
   assign in_msg_header_ready_and_o = ~header_v_r;
 
-  // has_data: start streaming data, keep the header in fifo and increment the address
-  // ~has_data: pass the header
-  wire has_data = payload_mask_p[out_header_lo.msg_type];
+  // Stream pump to handle all the details of properly forming the output BedRock stream message
+  logic fsm_ready_and_lo, fsm_v_li;
+  logic stream_cnt_lo, stream_done_lo;
+  bp_stream_pump_out
+    #(.bp_params_p(bp_params_p)
+      ,.stream_data_width_p(data_width_p)
+      ,.block_width_p(block_width_p)
+      ,.payload_width_p(payload_width_p)
+      ,.msg_stream_mask_p(payload_mask_p)
+      )
+     stream_pump_out
+      (.clk_i(clk_i)
+       ,.reset_i(reset_i)
+       ,.msg_header_o(out_msg_header_o)
+       ,.msg_data_o(out_msg_data_o)
+       ,.msg_v_o(out_msg_v_o)
+       ,.msg_last_o(out_msg_last_o)
+       ,.msg_ready_and_i(out_msg_ready_and_i)
+       ,.fsm_base_header_i(msg_header_li)
+       ,.fsm_data_i(in_msg_data_i)
+       ,.fsm_v_i(fsm_v_li)
+       ,.fsm_ready_and_o(fsm_ready_and_lo)
+       ,.stream_cnt_o(stream_cnt_lo)
+       ,.stream_done_o(stream_done_lo)
+       );
 
-  // passthrough data
-  assign out_msg_data_o = in_msg_data_i;
-  assign out_msg_v_o = header_v_lo & (in_msg_data_v_i | ~has_data);
-  assign in_msg_data_ready_and_o = out_msg_ready_and_i & out_msg_v_o & has_data;
-
-  bp_bedrock_out_msg_header_s out_msg_header_cast_o;
-  // increment the address for bp_stream
-  logic is_last_cnt;
-  if (stream_words_lp == 1)
-   begin: full_block_stream
-      assign is_last_cnt = out_msg_v_o;
-      assign out_msg_header_cast_o = out_header_lo;
-   end
-  else
-    begin: sub_block_stream
-      logic set_cnt, cnt_up, streaming_r;
-      logic [data_len_width_lp-1:0] first_cnt, last_cnt, current_cnt, stream_cnt;
-      wire [data_len_width_lp-1:0] num_stream = `BSG_MAX((1'b1 << out_header_lo.size) / out_data_bytes_lp, 1'b1);
-
-      bsg_counter_set_en
-       #(.max_val_p(stream_words_lp-1)
-       ,.reset_val_p(0))
-       data_counter
-        (.clk_i(clk_i)
-        ,.reset_i(reset_i)
-
-        ,.set_i(set_cnt)
-        ,.en_i(cnt_up)
-        ,.val_i(first_cnt+cnt_up)
-        ,.count_o(current_cnt)
-        );
-
-      bsg_dff_reset_set_clear
-       #(.width_p(1)
-       ,.clear_over_set_p(1))
-       streaming_reg
-        (.clk_i(clk_i)
-        ,.reset_i(reset_i)
-        ,.set_i(set_cnt)
-        ,.clear_i(out_msg_last_o)
-        ,.data_o(streaming_r)
-        );
-
-      assign first_cnt = out_header_lo.addr[stream_offset_width_lp+:data_len_width_lp];
-      assign last_cnt  = first_cnt + num_stream - 1'b1;
-
-      assign set_cnt = out_msg_ready_and_i & out_msg_v_o & has_data & ~streaming_r;
-      assign cnt_up = out_msg_ready_and_i & out_msg_v_o;
-
-      assign stream_cnt = set_cnt ? first_cnt : current_cnt;
-
-      wire single_beat = ~has_data | (first_cnt == last_cnt);
-      assign is_last_cnt = (stream_cnt == last_cnt) | single_beat;
-
-      always_comb
-        begin
-          out_msg_header_cast_o = out_header_lo;
-          out_msg_header_cast_o.addr = { out_header_lo.addr[paddr_width_p-1:stream_offset_width_lp+data_len_width_lp]
-                                       , {(data_len_width_lp>0){stream_cnt}}
-                                       , out_header_lo.addr[0+:stream_offset_width_lp] };
-        end
-    end
-
-  assign out_msg_last_o = out_msg_v_o & is_last_cnt;
-  assign out_msg_header_o = out_msg_header_cast_o;
-
-  //synopsys translate_off
-  initial
-    begin
-      assert (in_data_width_p == out_data_width_p)
-        else $error("Input data width should be identical with output data width");
-    end
-  //synopsys translate_on
+  // Stream pump needs valid header and, if required, data in same cycle
+  assign fsm_v_li = header_v_r & (~msg_has_data_li | in_msg_data_v_i);
+  // Accept data cycle after header arrives, but only if current message has data.
+  // This module does not support concurrent burst messages, so data must only be accepted
+  // if the buffered header indicates that data exists for the message.
+  assign in_msg_data_ready_and_o = header_v_r & msg_has_data_li & fsm_ready_and_lo;
 
 endmodule
 
