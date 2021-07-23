a4bd7bff868cefc1d7aecc090b543a795ed72e2c
Mark Wyse <muwyse@gmail.com>
2021-07-23 13:12:14 -0700

    header in bp_unicore

--- bp_top/src/v/bp_unicore.sv
+++ bp_top/src/v/bp_unicore.sv
@@ -1,3 +1,17 @@
+/**
+ *
+ * Name:
+ *   bp_unicore.sv
+ *
+ * Description:
+ *   This is the top level module for a unicore BlackParrot processor.
+ *
+ *   The unicore contains:
+ *   - a BlackParrot processor core and devices (config, clint, CCE loopback) in bp_unicore_lite
+ *   - L2 cache slice in bsg_cache
+ *   - core to cache adapter in bp_me_cce_to_cache
+ *
+ */
 
 `include "bp_common_defines.svh"
 `include "bp_fe_defines.svh"
