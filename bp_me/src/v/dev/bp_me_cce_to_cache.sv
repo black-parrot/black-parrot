2c388895828a5248615b2f470961f5fa7d5fceb2
Mark Wyse <muwyse@gmail.com>
2021-07-23 09:32:21 -0700

    minor update to cce_to_cache

--- bp_me/src/v/dev/bp_me_cce_to_cache.sv
+++ bp_me/src/v/dev/bp_me_cce_to_cache.sv
@@ -6,7 +6,7 @@
                     -----------------------------------------
       mem_cmd_i -->| --> pump_in --->  cache_pkt_sel --->    |--> cache_pkt_p
                    |                       |                 |
-  (mem_data_width) |   (mem_data_width)    | (l2_data_width) |  (l2_data_width)
+   (l2_data_width) |   (l2_data_width)     | (l2_data_width) |  (l2_data_width)
                    |                       |                 |
      mem_resp_o <--| <-- pump_out <--  bsg_bus_pack <----    |<-- data_i
                     -----------------------------------------
@@ -40,7 +40,7 @@ module bp_me_cce_to_cache
     input clk_i
     , input reset_i
 
-    // Stream interface
+    // BedRock Stream interface
     , input  [cce_mem_msg_header_width_lp-1:0] mem_cmd_header_i
     , input  [l2_data_width_p-1:0]             mem_cmd_data_i
     , input                                    mem_cmd_v_i
@@ -123,6 +123,7 @@ module bp_me_cce_to_cache
      ,.fsm_yumi_i(mem_cmd_yumi_li)
      ,.fsm_new_o(mem_cmd_new_lo)
      ,.fsm_done_o(mem_cmd_done_lo)
+     ,.fsm_last_o(/* unused */)
      );
 
   bp_local_addr_s local_addr_cast;
