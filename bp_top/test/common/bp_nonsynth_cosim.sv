96bf5730835094f0b80929c40e418684e01da60d
Dan Petrisko <petrisko@cs.washington.edu>
2021-07-26 01:57:07 -0700

    Fixes cosimulation for a non-replay D$ (#901)

--- bp_top/test/common/bp_nonsynth_cosim.sv
+++ bp_top/test/common/bp_nonsynth_cosim.sv
@@ -45,6 +45,9 @@ module bp_nonsynth_cosim
     , input                                   frd_w_v_i
     , input [rv64_reg_addr_width_gp-1:0]      frd_addr_i
     , input [dpath_width_gp-1:0]              frd_data_i
+
+    , input                                   wbuf_v
+    , input                                   uc_st_v
     );
 
   import "DPI-C" context function void dromajo_init(string cfg_f_name, int hartid, int ncpus, int memory_size, bit checkpoint, bit amo_en);
@@ -64,13 +67,20 @@ module bp_nonsynth_cosim
 
   bp_be_decode_s decode_r;
   bsg_dff_chain
-   #(.width_p($bits(bp_be_decode_s))
-     ,.num_stages_p(4)
-     )
+   #(.width_p($bits(bp_be_decode_s)), .num_stages_p(4))
    reservation_pipe
     (.clk_i(clk_i)
      ,.data_i(decode_i)
      ,.data_o(decode_r)
+     );
+
+  logic uc_st_r;
+  bsg_dff_chain
+   #(.width_p(1), .num_stages_p(2))
+   uc_st_pipe
+    (.clk_i(clk_i)
+     ,.data_i(uc_st_v)
+     ,.data_o(uc_st_r)
      );
 
   bp_be_commit_pkt_s commit_pkt_r;
@@ -85,6 +95,7 @@ module bp_nonsynth_cosim
      ,.data_o({is_debug_mode_r, commit_pkt_r})
      );
 
+  logic                     commit_fifo_ready_lo;
   logic                     commit_debug_r;
   logic                     instret_v_r;
   logic                     trap_v_r;
@@ -92,6 +103,7 @@ module bp_nonsynth_cosim
   rv64_instr_fmatype_s      commit_instr, commit_instr_r;
   logic                     commit_ird_w_v_r;
   logic                     commit_frd_w_v_r;
+  logic                     commit_st_v_r;
   logic [dword_width_gp-1:0] cause_r, mstatus_r;
   logic commit_fifo_v_lo, commit_fifo_yumi_li;
   wire instret_v_li = commit_pkt_r.instret;
@@ -99,20 +111,26 @@ module bp_nonsynth_cosim
   wire [instr_width_gp-1:0] commit_instr_li = commit_pkt_r.instr;
   wire commit_ird_w_v_li = instret_v_li & (decode_r.irf_w_v | decode_r.late_iwb_v);
   wire commit_frd_w_v_li = instret_v_li & (decode_r.frf_w_v | decode_r.late_fwb_v);
+  wire commit_st_v_li    = instret_v_li & decode_r.dcache_w_v & ~uc_st_r & ~commit_ird_w_v_li & ~commit_frd_w_v_li;
   wire trap_v_li = commit_pkt_r.exception | commit_pkt_r._interrupt;
   wire [dword_width_gp-1:0] cause_li = (priv_mode_i == `PRIV_MODE_M) ? mcause_i : scause_i;
   wire [dword_width_gp-1:0] mstatus_li = mstatus_i;
+  wire commit_fifo_v_li = instret_v_li | trap_v_li;
   bsg_fifo_1r1w_small
-   #(.width_p(3+vaddr_width_p+instr_width_gp+2+2*dword_width_gp), .els_p(128))
+   // Set really high to avoid reordering. But if we need it higher, we probably need
+   //   to implement a drain with a Nx clock. Basically the problem is that misses can
+   //   take several cycles, so each instruction that slips in there will not "catch up"
+   //   unless there's a true depedency stall, which are rare for many workloads
+   #(.width_p(3+vaddr_width_p+instr_width_gp+3+2*dword_width_gp), .els_p(128))
    commit_fifo
     (.clk_i(clk_i)
      ,.reset_i(reset_i)
 
-     ,.data_i({is_debug_mode_r, instret_v_li, trap_v_li, commit_pc_li, commit_instr_li, commit_ird_w_v_li, commit_frd_w_v_li, cause_li, mstatus_li})
-     ,.v_i(instret_v_li | trap_v_li)
-     ,.ready_o()
+     ,.data_i({is_debug_mode_r, instret_v_li, trap_v_li, commit_pc_li, commit_instr_li, commit_ird_w_v_li, commit_frd_w_v_li, commit_st_v_li, cause_li, mstatus_li})
+     ,.v_i(commit_fifo_v_li)
+     ,.ready_o(commit_fifo_ready_lo)
 
-     ,.data_o({commit_debug_r, instret_v_r, trap_v_r, commit_pc_r, commit_instr_r, commit_ird_w_v_r, commit_frd_w_v_r, cause_r, mstatus_r})
+     ,.data_o({commit_debug_r, instret_v_r, trap_v_r, commit_pc_r, commit_instr_r, commit_ird_w_v_r, commit_frd_w_v_r, commit_st_v_r, cause_r, mstatus_r})
      ,.v_o(commit_fifo_v_lo)
      ,.yumi_i(commit_fifo_yumi_li)
      );
@@ -128,19 +146,19 @@ module bp_nonsynth_cosim
       wire fill       = ird_w_v_i & (ird_addr_i == i);
       wire deallocate = commit_ird_w_v_r & (commit_instr_r.rd_addr == i) & commit_fifo_yumi_li;
       bsg_fifo_1r1w_small
-        #(.width_p(dword_width_gp), .els_p(128))
-        ird_fifo
-         (.clk_i(clk_i)
-          ,.reset_i(reset_i)
+       #(.width_p(dword_width_gp), .els_p(128))
+       ird_fifo
+        (.clk_i(clk_i)
+         ,.reset_i(reset_i)
 
-          ,.data_i(ird_data_i[0+:dword_width_gp])
-          ,.v_i(fill)
-          ,.ready_o()
+         ,.data_i(ird_data_i[0+:dword_width_gp])
+         ,.v_i(fill)
+         ,.ready_o()
 
-          ,.data_o(ird_data_r[i])
-          ,.v_o(ird_fifo_v_lo[i])
-          ,.yumi_i(deallocate)
-          );
+         ,.data_o(ird_data_r[i])
+         ,.v_o(ird_fifo_v_lo[i])
+         ,.yumi_i(deallocate)
+         );
     end
 
   for (genvar i = 0; i < rf_els_lp; i++)
@@ -148,19 +166,19 @@ module bp_nonsynth_cosim
       wire fill       = frd_w_v_i & (frd_addr_i == i);
       wire deallocate = commit_frd_w_v_r & (commit_instr_r.rd_addr == i) & commit_fifo_yumi_li;
       bsg_fifo_1r1w_small
-        #(.width_p(dpath_width_gp), .els_p(128))
-        ird_fifo
-         (.clk_i(clk_i)
-          ,.reset_i(reset_i)
+       #(.width_p(dpath_width_gp), .els_p(128))
+       ird_fifo
+        (.clk_i(clk_i)
+         ,.reset_i(reset_i)
 
-          ,.data_i(frd_data_i)
-          ,.v_i(fill)
-          ,.ready_o()
+         ,.data_i(frd_data_i)
+         ,.v_i(fill)
+         ,.ready_o()
 
-          ,.data_o(frd_data_r[i])
-          ,.v_o(frd_fifo_v_lo[i])
-          ,.yumi_i(deallocate)
-          );
+         ,.data_o(frd_data_r[i])
+         ,.v_o(frd_fifo_v_lo[i])
+         ,.yumi_i(deallocate)
+         );
 
       // The control bits control tininess, which is fixed in RISC-V
       wire [`floatControlWidth-1:0] control_li = `flControl_default;
@@ -175,9 +193,22 @@ module bp_nonsynth_cosim
          );
     end
 
-  assign commit_fifo_yumi_li = commit_fifo_v_lo & ((~commit_ird_w_v_r & ~commit_frd_w_v_r)
-                                                   | (commit_ird_w_v_r & ird_fifo_v_lo[commit_instr_r.rd_addr])
-                                                   | (commit_frd_w_v_r & frd_fifo_v_lo[commit_instr_r.rd_addr])
+  logic [`BSG_WIDTH(128)-1:0] wbuf_count_lo;
+  wire wbuf_v_lo = (wbuf_count_lo > '0);
+  bsg_counter_up_down
+   #(.max_val_p(128), .init_val_p(0), .max_step_p(1))
+   wbuf_counter
+    (.clk_i(clk_i)
+     ,.reset_i(reset_i)
+
+     ,.up_i(wbuf_v)
+     ,.down_i(commit_fifo_yumi_li & commit_st_v_r)
+     ,.count_o(wbuf_count_lo)
+     );
+
+  assign commit_fifo_yumi_li = commit_fifo_v_lo & ((~commit_ird_w_v_r | ird_fifo_v_lo[commit_instr_r.rd_addr])
+                                                   & (~commit_frd_w_v_r | frd_fifo_v_lo[commit_instr_r.rd_addr])
+                                                   & (~commit_st_v_r | wbuf_v_lo)
                                                    );
   wire commit_ird_li = commit_fifo_v_lo & (commit_ird_w_v_r & ird_fifo_v_lo[commit_instr_r.rd_addr]);
   wire commit_frd_li = commit_fifo_v_lo & (commit_frd_w_v_r & frd_fifo_v_lo[commit_instr_r.rd_addr]);
@@ -218,7 +249,7 @@ module bp_nonsynth_cosim
     else if (~commit_debug_r & cosim_en_i & commit_fifo_yumi_li & instret_v_r & commit_pc_r != '0)
       if (dromajo_step(mhartid_i, 64'($signed(commit_pc_r)), commit_instr_r, commit_frd_li ? frd_raw_li[commit_instr_r.rd_addr] : ird_data_r[commit_instr_r.rd_addr], mstatus_r))
         begin
-          $display("COSIM_FAIL");
+          $display("COSIM_FAIL: instruction mismatch");
           $finish();
         end
     else if (terminate)
@@ -252,6 +283,13 @@ module bp_nonsynth_cosim
         if (commit_fifo_yumi_li & commit_frd_w_v_r)
           $fwrite(file, "%x %x", commit_instr_r.rd_addr, frd_raw_li[commit_instr_r.rd_addr]);
         $fwrite(file, "\n");
+      end
+
+  always_ff @(negedge clk_i)
+    if (commit_fifo_v_li & ~commit_fifo_ready_lo)
+      begin
+        $display("COSIM_FAIL: commit fifo overrun");
+        $finish();
       end
 
 endmodule
