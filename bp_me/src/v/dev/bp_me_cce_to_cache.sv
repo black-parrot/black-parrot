9d96d3e8022adf4478322a71ccb182f34c4ef5d3
Mark Wyse <muwyse@gmail.com>
2021-09-01 13:46:57 -0700

    address reviews, split to encode/decode

--- bp_me/src/v/dev/bp_me_cce_to_cache.sv
+++ bp_me/src/v/dev/bp_me_cce_to_cache.sv
@@ -315,10 +315,11 @@ module bp_me_cce_to_cache
   localparam block_offset_lp = `BSG_SAFE_CLOG2(cce_block_width_p/8);
   localparam lg_lce_sets_lp = `BSG_SAFE_CLOG2(lce_sets_p);
   localparam lg_num_cce_lp = `BSG_SAFE_CLOG2(num_cce_p);
+  localparam int hash_offset_widths_lp[2:0] = '{(lg_lce_sets_lp-lg_num_cce_lp), lg_num_cce_lp, block_offset_lp};
   logic [daddr_width_p-1:0] cache_pkt_addr_lo;
-  bp_me_dram_hash
+  bp_me_dram_hash_encode
     #(.bp_params_p(bp_params_p)
-      ,.offset_widths_p('{(lg_lce_sets_lp-lg_num_cce_lp), lg_num_cce_lp, block_offset_lp})
+      ,.offset_widths_p(hash_offset_widths_lp)
       ,.addr_width_p(daddr_width_p)
       )
     mem_cmd_hash
