b34ea2c96473d0900dea92ba132b0ff6266b058c
Mark Wyse <muwyse@gmail.com>
2021-11-08 16:40:01 -0800

    move bedrock payload masks to bedrock package def

--- bp_me/src/include/bp_me_pkg.sv
+++ bp_me/src/include/bp_me_pkg.sv
@@ -9,12 +9,6 @@ package bp_me_pkg;
 
   import bp_common_pkg::*;
 
-  localparam mem_cmd_payload_mask_gp  = (1 << e_bedrock_mem_uc_wr) | (1 << e_bedrock_mem_wr) | (1 << e_bedrock_mem_amo);
-  localparam mem_resp_payload_mask_gp = (1 << e_bedrock_mem_uc_rd) | (1 << e_bedrock_mem_rd) | (1 << e_bedrock_mem_amo);
-  localparam lce_req_payload_mask_gp = (1 << e_bedrock_req_uc_wr);
-  localparam lce_cmd_payload_mask_gp = (1 << e_bedrock_cmd_data) | (1 << e_bedrock_cmd_uc_data);
-  localparam lce_resp_payload_mask_gp = (1 << e_bedrock_resp_wb);
-
   `include "bp_me_cce_pkgdef.svh"
   `include "bp_me_cce_inst_pkgdef.svh"
   `include "bp_me_axi_pkgdef.sv"
