2bc0669e4914a83e0712806c1afef7b55761871e
Mark Wyse <muwyse@gmail.com>
2022-02-17 15:18:10 -0800

    ci: weird_configs script

--- ci/weird_config.sh
+++ ci/weird_config.sh
@@ -58,18 +58,22 @@ make -C bp_top/syn clean
 let JOBS=${#cfgs[@]}
 let CORES_PER_JOB=${N}/${JOBS}+1
 
+# Build configs
+cmd_base="make -C bp_top/syn build.${SUFFIX} COSIM_P=1 DRAM=axi"
+parallel --jobs ${JOBS} --results regress_logs --progress "$cmd_base CFG={}" ::: "${cfgs[@]}"
+
 # Run the regression in parallel on each configuration
-cmd_base="make -C bp_top/syn build.${SUFFIX} sim.${SUFFIX} COSIM_P=1 SUITE=bp-tests PROG=hello_world DRAM=axi"
+cmd_base="make -C bp_top/syn sim.${SUFFIX} COSIM_P=1 SUITE=bp-tests PROG=hello_world DRAM=axi"
 echo "Running ${JOBS} jobs with ${CORES_PER_JOB} cores per job"
 parallel --jobs ${JOBS} --results regress_logs --progress "$cmd_base CFG={}" ::: "${cfgs[@]}"
 
 # Run a second set of tests
-cmd_base="make -C bp_top/syn build.${SUFFIX} sim.${SUFFIX} COSIM_P=1 SUITE=bp-tests PROG=cache_hammer DRAM=axi"
+cmd_base="make -C bp_top/syn sim.${SUFFIX} COSIM_P=1 SUITE=bp-tests PROG=cache_hammer DRAM=axi"
 echo "Running ${JOBS} jobs with ${CORES_PER_JOB} cores per job"
 parallel --jobs ${JOBS} --results regress_logs --progress "$cmd_base CFG={}" ::: "${cfgs[@]}"
 
 # Run a third set of tests
-cmd_base="make -C bp_top/syn build.${SUFFIX} sim.${SUFFIX} COSIM_P=1 SUITE=bp-tests PROG=stream_hammer DRAM=axi"
+cmd_base="make -C bp_top/syn sim.${SUFFIX} COSIM_P=1 SUITE=bp-tests PROG=stream_hammer DRAM=axi"
 echo "Running ${JOBS} jobs with ${CORES_PER_JOB} cores per job"
 parallel --jobs ${JOBS} --results regress_logs --progress "$cmd_base CFG={}" ::: "${cfgs[@]}"
 
