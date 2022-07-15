3c81604b65b08682bb33e6dce2cfa8ab8b93756f
Dan Petrisko <petrisko@cs.washington.edu>
2022-07-15 12:46:47 -0700

    Putting copyright headers in heach source file

--- bp_common/src/v/bsg_bus_pack.sv
+++ bp_common/src/v/bsg_bus_pack.sv
@@ -1,3 +1,14 @@
+// Copyright (c) 2022, University of Washington
+// Copyright and related rights are licensed under the BSD 3-Clause
+// License (the “License”); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at
+// https://github.com/black-parrot/black-parrot/LICENSE.
+// Unless required by applicable law or agreed to in writing, software,
+// hardware and materials distributed under this License is distributed
+// on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language
+// governing permissions and limitations under the License.
+
 
 `include "bsg_defines.v"
 
