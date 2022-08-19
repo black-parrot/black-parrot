0422f5b5397558b2b0c275e5b533ef49b5bb9d3d
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-08-19 22:40:58 +0500

    Replaced the generic global ctag_width parameter

--- bp_fe/test/tb/bp_fe_icache/wrapper.sv
+++ bp_fe/test/tb/bp_fe_icache/wrapper.sv
@@ -12,7 +12,7 @@ module wrapper
    , parameter fill_width_p = icache_fill_width_p
    `declare_bp_bedrock_lce_if_widths(paddr_width_p, lce_id_width_p, cce_id_width_p, lce_assoc_p)
    `declare_bp_bedrock_mem_if_widths(paddr_width_p, did_width_p, lce_id_width_p, lce_assoc_p)
-   `declare_bp_cache_engine_if_widths(paddr_width_p, ctag_width_p, icache_sets_p, icache_assoc_p, dword_width_gp, icache_block_width_p, icache_fill_width_p, icache)
+   `declare_bp_cache_engine_if_widths(paddr_width_p, icache_ctag_width_p, icache_sets_p, icache_assoc_p, dword_width_gp, icache_block_width_p, icache_fill_width_p, icache)
 
    , localparam cfg_bus_width_lp = `bp_cfg_bus_width(vaddr_width_p, hio_width_p, core_id_width_p, cce_id_width_p, lce_id_width_p)
    )
