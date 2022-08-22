8b9f75907fe106d392597c7f2f0f1d5fd1b28798
Kaelin Laundry <kaelinl@cs.washington.edu>
2022-08-22 12:47:36 -0700

    Introduce compressed_support_p config parameter
    Currently unused and defaults to disabled.

--- bp_common/src/include/bp_common_aviary_defines.svh
+++ bp_common/src/include/bp_common_aviary_defines.svh
@@ -107,6 +107,7 @@
     , localparam fe_cmd_fifo_els_p    = proc_param_lp.fe_cmd_fifo_els                              \
     , localparam muldiv_support_p     = proc_param_lp.muldiv_support                               \
     , localparam fpu_support_p        = proc_param_lp.fpu_support                                  \
+    , localparam compressed_support_p = proc_param_lp.compressed_support                           \
                                                                                                    \
     , localparam async_coh_clk_p        = proc_param_lp.async_coh_clk                              \
     , localparam coh_noc_max_credits_p  = proc_param_lp.coh_noc_max_credits                        \
@@ -200,6 +201,7 @@
           ,`bp_aviary_parameter_override(fe_cmd_fifo_els, override_cfg_mp, default_cfg_mp)         \
           ,`bp_aviary_parameter_override(muldiv_support, override_cfg_mp, default_cfg_mp)          \
           ,`bp_aviary_parameter_override(fpu_support, override_cfg_mp, default_cfg_mp)             \
+          ,`bp_aviary_parameter_override(compressed_support, override_cfg_mp, default_cfg_mp)      \
                                                                                                    \
           ,`bp_aviary_parameter_override(branch_metadata_fwd_width, override_cfg_mp, default_cfg_mp) \
           ,`bp_aviary_parameter_override(btb_tag_width, override_cfg_mp, default_cfg_mp)           \
