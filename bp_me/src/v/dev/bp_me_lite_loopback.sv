8388b4a8dadfa13165d3994b5f72aa279ba97d40
Mark Wyse <muwyse@gmail.com>
2023-04-04 22:59:35 -0700

    lite_loopback generalization

--- bp_me/src/v/dev/bp_me_lite_loopback.sv
+++ bp_me/src/v/dev/bp_me_lite_loopback.sv
@@ -17,38 +17,42 @@ module bp_me_lite_loopback
   import bp_me_pkg::*;
   #(parameter bp_params_e bp_params_p = e_bp_default_cfg
     `declare_bp_proc_params(bp_params_p)
-    `declare_bp_bedrock_mem_if_widths(paddr_width_p, did_width_p, lce_id_width_p, lce_assoc_p)
+    , parameter `BSG_INV_PARAM(payload_width_p)
+    , parameter `BSG_INV_PARAM(data_width_p)
+    `declare_bp_bedrock_if_widths(paddr_width_p, payload_width_p, bp)
     )
    (input                                            clk_i
     , input                                          reset_i
 
-    , input [mem_fwd_header_width_lp-1:0]            mem_fwd_header_i
-    , input [dword_width_gp-1:0]                     mem_fwd_data_i
-    , input                                          mem_fwd_v_i
-    , output logic                                   mem_fwd_ready_and_o
+    , input [bp_header_width_lp-1:0]                 in_msg_header_i
+    , input [data_width_p-1:0]                       in_msg_data_i
+    , input                                          in_msg_v_i
+    , output logic                                   in_msg_ready_and_o
 
-    , output logic [mem_rev_header_width_lp-1:0]     mem_rev_header_o
-    , output logic [dword_width_gp-1:0]              mem_rev_data_o
-    , output logic                                   mem_rev_v_o
-    , input                                          mem_rev_ready_and_i
+    , output logic [bp_header_width_lp-1:0]          out_msg_header_o
+    , output logic [data_width_p-1:0]                out_msg_data_o
+    , output logic                                   out_msg_v_o
+    , input                                          out_msg_ready_and_i
     );
 
-  // Used to decouple to help prevent deadlock
+  wire unused = &{in_msg_data_i};
+  assign out_msg_data_o = '0;
+
   bsg_one_fifo
-   #(.width_p(mem_fwd_header_width_lp))
+   #(.width_p(bp_header_width_lp))
    loopback_buffer
     (.clk_i(clk_i)
      ,.reset_i(reset_i)
 
-     ,.data_i(mem_fwd_header_i)
-     ,.v_i(mem_fwd_v_i)
-     ,.ready_o(mem_fwd_ready_and_o)
+     ,.data_i(in_msg_header_i)
+     ,.v_i(in_msg_v_i)
+     ,.ready_o(in_msg_ready_and_o)
 
-     ,.data_o(mem_rev_header_o)
-     ,.v_o(mem_rev_v_o)
-     ,.yumi_i(mem_rev_ready_and_i & mem_rev_v_o)
+     ,.data_o(out_msg_header_o)
+     ,.v_o(out_msg_v_o)
+     ,.yumi_i(out_msg_ready_and_i & out_msg_v_o)
      );
-  assign mem_rev_data_o = '0;
 
 endmodule
 
+`BSG_ABSTRACT_MODULE(bp_me_lite_loopback)
