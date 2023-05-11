892134b2b941387dfc6f88968b8f92018cd4f7e1
Dan Petrisko <petrisko@cs.washington.edu>
2023-05-11 15:29:42 -0700

    Increase IO credits

--- bp_common/src/include/bp_common_aviary_cfg_pkgdef.svh
+++ bp_common/src/include/bp_common_aviary_cfg_pkgdef.svh
@@ -308,20 +308,20 @@
       ,coh_noc_flit_width  : 64
       ,coh_noc_cid_width   : 2
       ,coh_noc_len_width   : 4
-      ,coh_noc_max_credits : 8
+      ,coh_noc_max_credits : 32
 
       ,async_mem_clk         : 0
       ,mem_noc_flit_width    : 64
       ,mem_noc_cid_width     : 2
       ,mem_noc_len_width     : 4
-      ,mem_noc_max_credits   : 8
+      ,mem_noc_max_credits   : 32
 
       ,async_io_clk         : 0
       ,io_noc_flit_width    : 64
       ,io_noc_cid_width     : 2
       ,io_noc_did_width     : 3
       ,io_noc_len_width     : 4
-      ,io_noc_max_credits   : 16
+      ,io_noc_max_credits   : 32
       };
 
   // BP_CUSTOM_DEFINES_PATH can be set to a file which has the custom defines below set
