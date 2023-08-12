aa0e865b64468ac43edbabd2658d90b01a394036
Dan Petrisko <petrisko@cs.washington.edu>
2023-08-12 11:58:14 -0700

    Bugfix

--- bp_be/src/v/bp_be_dcache/bp_be_dcache.sv
+++ bp_be/src/v/bp_be_dcache/bp_be_dcache.sv
@@ -564,7 +564,7 @@ module bp_be_dcache
   logic wbuf_v_li, wbuf_v_lo, wbuf_force_lo, wbuf_snoop_match_lo, wbuf_yumi_li;
 
   assign wbuf_v_li = v_tv_r
-    & decode_tv_r.store_op & ~sc_fail_tv & ~uncached_op_tv_r
+    & decode_tv_r.store_op & store_hit_tv & ~sc_fail_tv & ~uncached_op_tv_r
     & ~any_miss_tv;
 
   //
