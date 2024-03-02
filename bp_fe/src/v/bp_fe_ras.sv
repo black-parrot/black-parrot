c0a071da851877bdca49458ec54ecc7f31660103
Dan Petrisko <petrisko@cs.washington.edu>
2024-03-01 19:47:57 -0800

    RAS checkpointing enhancement (#1213)

--- bp_fe/src/v/bp_fe_ras.sv
+++ bp_fe/src/v/bp_fe_ras.sv
@@ -17,61 +17,59 @@ module bp_fe_ras
    , output logic                       init_done_o
 
    , input                              restore_i
-   , input [ras_idx_width_p-1:0]        w_base_i
-   , input [ras_idx_width_p-1:0]        w_cnt_i
+   , input [ras_idx_width_p-1:0]        w_next_i
+   , input [ras_idx_width_p-1:0]        w_tos_i
 
    , input                              call_i
    , input [vaddr_width_p-1:0]          addr_i
 
    , output logic                       v_o
    , output logic [vaddr_width_p-1:0]   tgt_o
-   , output logic [ras_idx_width_p-1:0] base_o
-   , output logic [ras_idx_width_p-1:0] cnt_o
+   , output logic [ras_idx_width_p-1:0] next_o
+   , output logic [ras_idx_width_p-1:0] tos_o
    , input                              return_i
    );
 
   localparam ras_els_lp = 2**ras_idx_width_p;
-  logic [ras_idx_width_p-1:0] bptr_n, bptr_r;
-  logic [ras_idx_width_p-1:0] cnt_n, cnt_r;
+  logic [ras_idx_width_p-1:0] next_n, next_r;
+  logic [ras_idx_width_p-1:0] tos_n, tos_r;
+  logic [ras_idx_width_p-1:0] nos_lo;
+  logic [vaddr_width_p-1:0] tgt_lo;
 
-  wire [ras_idx_width_p-1:0] wptr = bptr_r + cnt_r; // wptr leads the base by the count
-  wire [ras_idx_width_p-1:0] rptr = wptr - 1'b1;    // rptr always lags wptr by 1
-
-  // Assume POT RAS for now, implement circular pointers if this is onerous
-  wire empty = (cnt_r == '0);
-  wire full  = (cnt_r == '1);
-
-  // We assume overflow is desired (to get latest entries)
-  // We assume underflow is benign (would need RAS/
-  assign bptr_n = restore_i ? w_base_i : (bptr_r + (call_i & ~return_i & full));
-  assign cnt_n  = restore_i ? w_cnt_i  : (cnt_r + (call_i & ~full) - (return_i & ~empty));
-  bsg_dff_reset_en
+  // Algorithm taken from "Recovery Requirements of Branch Prediction Storage
+  //   Structures in the Presence of Mispredicted-Path Execution"
+  assign next_n = restore_i ? w_next_i : call_i ? (next_r+1'b1) :                     next_r;
+  assign tos_n  = restore_i ? w_tos_i  : call_i ? (next_r+1'b0) : return_i ? nos_lo : tos_r;
+  bsg_dff_reset
    #(.width_p(2*ras_idx_width_p))
    ptr_reg
     (.clk_i(clk_i)
      ,.reset_i(reset_i)
-     ,.en_i(restore_i | call_i | return_i)
-     ,.data_i({bptr_n, cnt_n})
-     ,.data_o({bptr_r, cnt_r})
+     ,.data_i({next_n, tos_n})
+     ,.data_o({next_r, tos_r})
      );
 
   // Needs to push/pop at the same time to comply with RISC-V hints, preventing
   //   hardening. But, we expect this to be a fairly small structure
   bsg_mem_1r1w
-   #(.width_p(vaddr_width_p), .els_p(ras_els_lp), .read_write_same_addr_p(1))
+   #(.width_p(ras_idx_width_p+vaddr_width_p), .els_p(ras_els_lp), .read_write_same_addr_p(1))
    mem
     (.w_clk_i(clk_i)
      ,.w_reset_i(reset_i)
      ,.w_v_i(call_i)
-     ,.w_addr_i(wptr)
-     ,.w_data_i(addr_i)
+     ,.w_addr_i(next_r)
+     ,.w_data_i({tos_r, addr_i})
      ,.r_v_i(return_i)
-     ,.r_addr_i(rptr)
-     ,.r_data_o(tgt_o)
+     ,.r_addr_i(tos_r)
+     ,.r_data_o({nos_lo, tgt_lo})
      );
-  assign base_o = bptr_r;
-  assign cnt_o = cnt_r;
-  assign v_o = ~empty;
+  assign tgt_o = tgt_lo;
+  assign next_o = next_r;
+  assign tos_o = tos_r;
+
+  // Keeping track is more overhead than correcting misaligned stacks. See:
+  // "Improving Prediction for Procedure Returns with Return-Address-Stack Repair Mechanisms"
+  assign v_o = 1'b1;
 
   // We use count for valid, so we're immediately ready to go
   assign init_done_o = 1'b1;
