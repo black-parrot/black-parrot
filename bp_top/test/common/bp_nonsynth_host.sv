50cf2e16c794d0d2df753efc6390afc9e6ab9f2f
Mark Wyse <muwyse@gmail.com>
2024-11-23 10:47:39 -0800

    64b aligned data return from nonsynth host bootrom

--- bp_top/test/common/bp_nonsynth_host.sv
+++ bp_top/test/common/bp_nonsynth_host.sv
@@ -37,9 +37,26 @@ module bp_nonsynth_host
    , output logic [bedrock_fill_width_p-1:0]        mem_rev_data_o
    , output logic                                   mem_rev_v_o
    , input                                          mem_rev_ready_and_i
+
+   , output logic                                   icache_trace_en_o
+   , output logic                                   dcache_trace_en_o
+   , output logic                                   lce_trace_en_o
+   , output logic                                   cce_trace_en_o
+   , output logic                                   dram_trace_en_o
+   , output logic                                   vm_trace_en_o
+   , output logic                                   cmt_trace_en_o
+   , output logic                                   core_profile_en_o
+   , output logic                                   pc_gen_trace_en_o
+   , output logic                                   pc_profile_en_o
+   , output logic                                   branch_profile_en_o
+   , output logic                                   cosim_en_o
+   , output logic                                   dev_trace_en_o
+   , output logic [num_core_p-1:0]                  finish_o
    );
 
-  import "DPI-C" context function int getchar();
+  import "DPI-C" context function void start();
+  import "DPI-C" context function int scan();
+  import "DPI-C" context function void pop();
 
   integer tmp;
   integer stdout[num_core_p];
@@ -55,6 +72,7 @@ module bp_nonsynth_host
         end
       stdout_global = $fopen("stdout_global.txt", "w");
       signature = $fopen("DUT-blackparrot.signature", "w");
+      start();
     end
 
   localparam bedrock_reg_els_lp = 8;
@@ -90,7 +108,7 @@ module bp_nonsynth_host
   wire [hio_width_p-1:0] hio_id = mem_fwd_header_li.addr[paddr_width_p-1-:hio_width_p];
   always_comb
     if (mem_fwd_v_i & (hio_id != '0))
-      $display("[BSG-WARNING]: Accessing hio %0h. Sending loopback message!", hio_id);
+      $display("Warning: Accessing hio %0h. Sending loopback message!", hio_id);
 
   // for some reason, VCS doesn't like finish_w_v_li << addr_core_enc
   wire [num_core_p-1:0] finish_set = finish_w_v_li ? (1'b1 << addr_core_enc) : 1'b0;
@@ -104,6 +122,7 @@ module bp_nonsynth_host
      ,.clear_i('0)
      ,.data_o(finish_r)
      );
+  assign finish_o = finish_r | finish_set;
 
   integer ret;
   logic [7:0] ch;
@@ -124,7 +143,8 @@ module bp_nonsynth_host
       end
 
       if (getchar_r_v_li) begin
-          ch <= getchar();
+          ch <= scan();
+          pop();
       end
 
       if (mem_fwd_ready_and_o & mem_fwd_v_i & (hio_id != '0))
@@ -133,14 +153,10 @@ module bp_nonsynth_host
         begin
           // PASS when returned value in finish packet is zero
           if (finish_set[i] & (data_lo[0+:8] == 8'(0)))
-            $display("[BSG-INFO]: CORE%0x FINISH PASS", i);
+            $display("[CORE%0x FSH] PASS", i);
           // FAIL when returned value in finish packet is non-zero
           if (finish_set[i] & (data_lo[0+:8] != 8'(0)))
-            begin
-              $display("[BSG-FAIL]: CORE%0x FINISH FAIL", i);
-              $finish();
-            end
-            
+            $display("[CORE%0x FSH] FAIL", i);
         end
 
       if (signature_w_v_li)
@@ -152,7 +168,7 @@ module bp_nonsynth_host
 
       if (&finish_r)
         begin
-          $display("[BSG-PASS]: All cores finished! Terminating...");
+          $display("All cores finished! Terminating...");
           $finish();
         end
     end
@@ -165,6 +181,31 @@ module bp_nonsynth_host
         $fclose(stdout[j]);
       $system("stty echo");
     end
+
+  localparam bootrom_width_p = 64;
+  localparam bootrom_els_p = 1024;
+  localparam lg_bootrom_els_lp = `BSG_SAFE_CLOG2(bootrom_els_p);
+  bit [bootrom_width_p-1:0] bootrom_data_lo;
+  bit [lg_bootrom_els_lp-1:0] bootrom_addr_li;
+  bit [7:0] bootrom_mem [0:8*bootrom_els_p-1];
+
+  initial $readmemh("bootrom.mem", bootrom_mem);
+  // TODO: account for size_lo from the bedrock message? Or is it safe to just do 64b aligned?
+  // bootrom is logically 64b wide x 1024 elements
+  // assuming 64b icache data width, should return 64b-aligned data
+  //assign bootrom_addr_li = addr_lo[2+:lg_bootrom_els_lp];
+  assign bootrom_addr_li = addr_lo[3+:lg_bootrom_els_lp];
+
+  assign bootrom_data_lo = {
+    bootrom_mem[8*bootrom_addr_li+7]
+    ,bootrom_mem[8*bootrom_addr_li+6]
+    ,bootrom_mem[8*bootrom_addr_li+5]
+    ,bootrom_mem[8*bootrom_addr_li+4]
+    ,bootrom_mem[8*bootrom_addr_li+3]
+    ,bootrom_mem[8*bootrom_addr_li+2]
+    ,bootrom_mem[8*bootrom_addr_li+1]
+    ,bootrom_mem[8*bootrom_addr_li+0]
+    };
 
   localparam param_els_lp = `BSG_CDIV($bits(proc_param_lp),word_width_gp);
   localparam lg_param_els_lp = `BSG_SAFE_CLOG2(param_els_lp);
@@ -184,11 +225,26 @@ module bp_nonsynth_host
      );
   wire [bedrock_block_width_p-1:0] paramrom_final_lo = {bedrock_block_width_p/word_width_gp{paramrom_data_lo}};
 
+  // TODO: Add dynamic enable
+  assign icache_trace_en_o   = icache_trace_p;
+  assign dcache_trace_en_o   = dcache_trace_p;
+  assign lce_trace_en_o      = lce_trace_p;
+  assign cce_trace_en_o      = cce_trace_p;
+  assign dram_trace_en_o     = dram_trace_p;
+  assign vm_trace_en_o       = vm_trace_p;
+  assign cmt_trace_en_o      = cmt_trace_p;
+  assign core_profile_en_o   = core_profile_p;
+  assign pc_gen_trace_en_o   = pc_gen_trace_p;
+  assign pc_profile_en_o     = pc_profile_p;
+  assign branch_profile_en_o = br_profile_p;
+  assign cosim_en_o          = cosim_p & ~&finish_r;
+  assign dev_trace_en_o      = dev_trace_p;
+
   assign data_li[0] = '0;
   assign data_li[1] = '0;
   assign data_li[2] = ch;
   assign data_li[3] = finish_r;
-  assign data_li[4] = '0;
+  assign data_li[4] = bootrom_data_lo;
   assign data_li[5] = paramrom_final_lo;
 
 endmodule
