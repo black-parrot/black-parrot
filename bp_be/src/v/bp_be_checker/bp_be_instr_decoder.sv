1e99b3177f4d5b6ddc0db7c68308997ff178ebe6
Flavien Solt <flavien@nus.edu.sg>
2026-08-22 13:42:28 +0800

    bp_be: Restrict satp access by privilege

--- bp_be/src/v/bp_be_checker/bp_be_instr_decoder.sv
+++ bp_be/src/v/bp_be_checker/bp_be_instr_decoder.sv
@@ -528,7 +528,8 @@ module bp_be_instr_decoder
                     ,`CSR_ADDR_FRM      : illegal_instr_o = !decode_info_cast_i.fpu_en;
                     `CSR_ADDR_CYCLE     : illegal_instr_o = !decode_info_cast_i.cycle_en;
                     `CSR_ADDR_INSTRET   : illegal_instr_o = !decode_info_cast_i.instret_en;
-                    `CSR_ADDR_SATP      : illegal_instr_o = decode_info_cast_i.s_mode & decode_info_cast_i.tvm;
+                    `CSR_ADDR_SATP      : illegal_instr_o = decode_info_cast_i.u_mode
+                                                           | (decode_info_cast_i.s_mode & decode_info_cast_i.tvm);
                     `CSR_ADDR_PMPCFG0
                     ,`CSR_ADDR_PMPCFG2
                     ,`CSR_ADDR_PMPADDR  : illegal_instr_o = num_pmp_p == 0;
@@ -822,4 +823,3 @@ module bp_be_instr_decoder
     end
 
 endmodule
-
